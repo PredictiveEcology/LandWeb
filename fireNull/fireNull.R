ee5fd0b4bb90bb74eb34880b459a6ea58f06d40f
SteveCumming <stevec.boreal@gmail.com>
2016-08-16 15:32:08 -0700

    remove spurious code from .init event.
    error check; verify naming conventions; update .Rmd a litte

--- fireNull/fireNull.R
+++ fireNull/fireNull.R
@@ -14,7 +14,7 @@ defineModule(sim, list(
   citation = list("citation.bib"),
   documentation = list("README.txt", "fireNull.Rmd"),
   reqdPkgs = list("raster","sp"),
-  parameters = rbind(
+  parameters = rbind( #should initial times be 0 or 1?
     #defineParameter("paramName", "paramClass", value, min, max, "parameter description")),
     #defineParameter("pBurn", "numeric", 0.01, 0, 1, desc="the rate of burn"),
     defineParameter("doAgeMapping", "logical", FALSE, TRUE, FALSE, desc="keep track of time since fire?"),
@@ -27,6 +27,8 @@ defineModule(sim, list(
     defineParameter(".statsInitialTime", "numeric", 0, NA, NA, desc="This describes the simulation time at which the first stats event should occur")
   ),
   inputObjects = data.frame(
+    #should LCC05, the fire regime shapefile and the study area raster be specified?
+    #they should be loaded by the parent, and are needed for init.
     objectName = "",
     objectClass = "",
     sourceURL = "",
@@ -34,7 +36,7 @@ defineModule(sim, list(
     stringsAsFactors = FALSE
   ),
   outputObjects = data.frame(
-    objectName = c("timeSinceFireMap","burnLoci","currentBurnMap"),
+    objectName = c("rasterTimeSinceFire","burnLoci","rasterCurrentBurn"),
     objectClass = c("RasterLayer","vector", "RasterLayer"),
     other = NA_character_,
     stringsAsFactors = FALSE
@@ -59,19 +61,17 @@ doEvent.fireNull = function(sim, eventTime, eventType, debug = FALSE) {
     if (params(sim)$fireNull$doAgeMapping == TRUE){
       Plot(sim$timeSinceFireMap)
     }
-    #Plot(objectFromModule) # uncomment this, replace with object to plot
-    # schedule future event(s)
-    
     # e.g.,
-    sim <- scheduleEvent(sim, params(sim)$fireNull$.plotInitialTime, "fireNull", "plot")
+    sim <- scheduleEvent(sim, time(sim) + params(sim)$fireNull$.plotInterval, "fireNull", "plot")
     # ! ----- STOP EDITING ----- ! #
   } else if (eventType == "save") {
+    sim <- scheduleEvent(sim, time(sim) + params(sim)$fireNull$.saveInterval, "fireNull", "save")
   } else if (eventType == "burn") {
     sim <- fireNullBurn(sim)
-    #do some book-keeping not part of the actual fire process
+    #do some book-keeping, not part of the actual fire process
     if (params(sim)$fireNull$doAgeMapping == TRUE){
-      sim$timeSinceFireMap <- sim$timeSinceFireMap + 1
-      sim$timeSinceFireMap[sim$burnLoci] = 0 
+      sim$rasterTimeSinceFire <- sim$rasterTimeSinceFire + 1
+      sim$rasterTimeSinceFire[sim$burnLoci] = 0 
     }
     #schedule next burn event
     sim <- scheduleEvent(sim, time(sim) + params(sim)$fireNull$returnInterval, "fireNull", "burn")
@@ -94,14 +94,44 @@ doEvent.fireNull = function(sim, eventTime, eventType, debug = FALSE) {
 ### template initialization
 fireNullInit <- function(sim) {
   
-  sim$currentBurnMap <- sim$burnProbMap * 0
+  if (!exists(rasterBurnProb, where=envir(sim))){
+    pBurn <- 1/sim$shapeFileFireRegime$fireReturnInterval
+    sim$rasterBurnProb <- rasterize(sim$shapeFileFireRegime,sim$rasterStudyArea,field=pBurn, mask=TRUE)
+  }
+  ##THIS should move to parent .init
+  if (!exists(rasterFlammable, where=envir(sim))){
+    nonFlammClasses<-c(36,37,38,39)
+    oldClass <- 0:39
+    newClass <- ifelse(oldClass %in% nonFlammClasses,1,0)   #1 codes for non flammable 
+    #see mask argument for SpaDES::spread()
+    flammableTable <- cbind(oldClass, newClass)
+    #according to Yong, Canada Landcover 2005 is loaded as LCC05 
+    sim$rasterFlammable <- ratify(reclassify(sim$LCC05, flammableTable,count=TRUE))
+    setColors(sim$rasterFlammable,n=2) <- colorRampPalette(c("blue", "red"))(2) 
+  }
+  #doing this here ensures non flammabe cells are accounted for, 
+  #no matter when/where rasterBurnProb and rasterFlammable are created.
+  sim$rasterBurnProb[which(sim$rasterFlammable == 0)] <- 0 #this could turn some NAs to 0s.
+  sim$rasterCurrentBurn <- sim$rasterBurnProb * 0 #this conserves NAs
   
   if (params(sim)$fireNull$doAgeMapping == TRUE){
-    sim$timeSinceFireMap <- sim$burnProbMap * 0
+    #ideally, we would have loaded an actual initial age map based e.g.
+    #on inventory and or the national product that BEACONs uses 
+    #(see the BEACONs modules for details and source)
+    sim$rasterTimeSinceFire <- sim$rasterBurnProb * 0 #this conserves NAs
     #assign legend and colours if you are serious
   }
   
-  sim$fireNullStats<-list(N=numeric(0),p=numeric(0),rate=numeric(0))
+  
+  #for any stats, we need to caculate how many burnable cells there are
+  N<- sum(!is.na(sim$rasterBurnProb)) 
+  N<- N - which(sim$rasterBurnProb == 0) # we will "mask" the lakes etc. with 0, not NA
+  sim$nBurnableCells <- N
+  sim$burnLoci <- vector("numeric")
+  ##
+  
+  
+  sim$fireNullStats<-list(N=numeric(0),rate=numeric(0))
   
   return(invisible(sim))
 }
@@ -129,14 +159,11 @@ fireNullPlot <- function(sim) {
 
 fireNullBurn <- function(sim) {
   # ! ----- EDIT BELOW ----- ! #
-  #note that this assumes square maps with no holes.
-  #we could filyter out the NAs and 0s, 
-  #but I bet it is faster to ignore them. 
   
-  N<-prod(dim(sim$burnProbMap))
-  sim$currentBurnMap<-sim$currentBurnMap*0 #zero, but preserve NAs
-  sim$burnLoci<-which(runif(N) < sim$burnProbMap)
-  sim$currentBurnMap[sim$burnLoci]<-1
+  N<-prod(dim(sim$rasterBurnProb))
+  sim$rasterCurrentBurn<-sim$rasterCurrentBurn*0 #zero, but preserve NAs
+  sim$burnLoci<-which(runif(N) < sim$rasterBurnProb) #this ignores any NAs in the map.
+  sim$rasterCurrentBurn[sim$burnLoci]<-1 #mark as burned.
   
   return(invisible(sim))
 }
@@ -145,10 +172,11 @@ fireNullStatsF<-function(sim){
   N<- sim$nBurnableCells
   
   sim$fireNullStats$rate<-c(sim$fireNullStats$rate,length(sim$burnLoci)/N)
-  sim$fireNullStats$p<-c(sim$fireNullStats$p,params(sim)$fireNull$pBurn)
+  #sim$fireNullStats$p<-c(sim$fireNullStats$p,params(sim)$fireNull$pBurn)
   sim$fireNullStats$N<-c(sim$fireNullStats$N,length(sim$burnLoci))
   return(invisible(sim))
 }
+
 .init = function(sim) {
   
   if (!exists(shapeFileFireRegime,where=envir(sim)) ||
@@ -157,30 +185,9 @@ fireNullStatsF<-function(sim){
     stop("missing or invalid shapefile: how did you even get here?")
   }
   
-  pBurn <- 1/sim$shapeFileFireRegime$fireReturnInterval
-  sim$burnProbMap <- rasterise(sim$shapeFileFireRegime,sim$studyAreaRaster,field=pBurn, mask=TRUE)
- 
-  ##THIS should move to parent .init
-  if (exists(flammableMap, where=envir(sim))){
-    nonFlammClasses<-c(36,37,38,39)
-    oldClass <- 0:39
-    newClass <- ifelse(oldClass %in% nonFlammClasses,1,0)   #1 codes for non flammable 
-    #see mask argument for SpaDES::spread()
-    flammableTable <- cbind(oldClass, newClass)
-    sim$flammableMap <- ratify(reclassify(sim$vegMap, flammableTable,count=TRUE))
-    setColors(sim$flammableMap,n=2) <- colorRampPalette(c("blue", "red"))(2) 
-    
-    sim$burnProbMap[which(sim$flammableMap == 0)] <- 0 #this could turn some NAs to 0s. 
-  }
-  
-  #for any stats, we need to caclulate how many burnable cells there are
-  N<- sum(!is.na(sim$burnProbMap)) 
-  N<- N - which(sim$burnProbMap == 0) # we will "mask" the lakes etc. with 0, not NA
-  sim$nBurnableCells <- N
-  sim$burnLoci <- vector("numeric")
-  ##
   
  
+  
   return(invisible(sim))
 }
 ### add additional events as needed by copy/pasting from above
