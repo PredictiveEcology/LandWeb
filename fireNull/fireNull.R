60ad08fb45af73596163160d25a3ccca77d254a8
SteveCumming <stevec.boreal@gmail.com>
2016-10-20 17:08:27 -0400

    Major design change to prototype LandWEB fire module
    Broken down into three modules, partial documentation, some caching.

--- fireNull/fireNull.R
+++ fireNull/fireNull.R
@@ -13,38 +13,34 @@ defineModule(sim, list(
   timeunit = "year",
   citation = list("citation.bib"),
   documentation = list("README.txt", "fireNull.Rmd"),
-  reqdPkgs = list("raster","sp"),
+  reqdPkgs = list("raster"),
   parameters = rbind( #should initial times be 0 or 1?
     #defineParameter("paramName", "paramClass", value, min, max, "parameter description")),
-    #defineParameter("pBurn", "numeric", 0.01, 0, 1, desc="the rate of burn"),
-    defineParameter("doAgeMapping", "logical", FALSE, TRUE, FALSE, desc="keep track of time since fire?"),
+    defineParameter("doAgeMapping", "logical", FALSE, FALSE, TRUE, desc="keep track of time since fire?"),
     defineParameter("returnInterval", "numeric", 1.0, NA, NA, desc="interval between main events"),
     defineParameter("startTime","numeric", 0, NA, NA, desc="time of first burn event"),
     defineParameter(".plotInitialTime", "numeric", NA, NA, NA, desc="This describes the simulation time at which the first plot event should occur"),
     defineParameter(".plotInterval", "numeric", NA, NA, NA, desc="This describes the simulation time at which the first plot event should occur"),
     defineParameter(".saveInitialTime", "numeric", NA, NA, NA, desc="This describes the simulation time at which the first save event should occur"),
     defineParameter(".saveInterval", "numeric", NA, NA, NA, desc="This describes the simulation time at which the first save event should occur"),
-    defineParameter(".statsInitialTime", "numeric", 0, NA, NA, desc="This describes the simulation time at which the first stats event should occur")
+    defineParameter(".statsInitialTime", "numeric", NA, NA, NA, desc="This describes the simulation time at which the first stats event should occur")
   ),
   inputObjects = data.frame(
     #should LCC05, the fire regime shapefile and the study area raster be specified?
     #they should be loaded by the parent, and are needed for init.
-    objectName = "",
-    objectClass = "",
-    sourceURL = "",
+    objectName = c("rstFlammable","rstBurnProb"),
+    objectClass = c("RasterLayer","RasterLayer"),
+    sourceURL = c("",""),
     other = NA_character_,
     stringsAsFactors = FALSE
   ),
   outputObjects = data.frame(
-    objectName = c("rasterTimeSinceFire","burnLoci","rasterCurrentBurn"),
-    objectClass = c("RasterLayer","vector", "RasterLayer"),
+    objectName = c("burnLoci","rstCurrentBurn","rstTimeSinceFire"),
+    objectClass = c("vector", "RasterLayer", "RasterLayer"),
     other = NA_character_,
     stringsAsFactors = FALSE
   )
 ))
-
-## event types
-#   - type `init` is required for initialiazation
 
 doEvent.fireNull = function(sim, eventTime, eventType, debug = FALSE) {
   if (eventType == "init") {
@@ -61,6 +57,7 @@ doEvent.fireNull = function(sim, eventTime, eventType, debug = FALSE) {
     if (params(sim)$fireNull$doAgeMapping == TRUE){
       Plot(sim$timeSinceFireMap)
     }
+    Plot(sim$rstCurrentBurn)
     # e.g.,
     sim <- scheduleEvent(sim, time(sim) + params(sim)$fireNull$.plotInterval, "fireNull", "plot")
     # ! ----- STOP EDITING ----- ! #
@@ -70,7 +67,7 @@ doEvent.fireNull = function(sim, eventTime, eventType, debug = FALSE) {
     sim <- fireNullBurn(sim)
     #do some book-keeping, not part of the actual fire process
     if (params(sim)$fireNull$doAgeMapping == TRUE){
-      sim$rasterTimeSinceFire <- sim$rasterTimeSinceFire + 1
+      sim$rasterTimeSinceFire <- sim$rasterTimeSinceFire + params(sim)$fireNull$returnInterval
       sim$rasterTimeSinceFire[sim$burnLoci] = 0 
     }
     #schedule next burn event
@@ -93,44 +90,27 @@ doEvent.fireNull = function(sim, eventTime, eventType, debug = FALSE) {
 
 ### template initialization
 fireNullInit <- function(sim) {
-  
-  if (!exists(rasterBurnProb, where=envir(sim))){
-    pBurn <- 1/sim$shapeFileFireRegime$fireReturnInterval
-    sim$rasterBurnProb <- rasterize(sim$shapeFileFireRegime,sim$rasterStudyArea,field=pBurn, mask=TRUE)
-  }
-  ##THIS should move to parent .init
-  if (!exists(rasterFlammable, where=envir(sim))){
-    nonFlammClasses<-c(36,37,38,39)
-    oldClass <- 0:39
-    newClass <- ifelse(oldClass %in% nonFlammClasses,1,0)   #1 codes for non flammable 
-    #see mask argument for SpaDES::spread()
-    flammableTable <- cbind(oldClass, newClass)
-    #according to Yong, Canada Landcover 2005 is loaded as LCC05 
-    sim$rasterFlammable <- ratify(reclassify(sim$LCC05, flammableTable,count=TRUE))
-    setColors(sim$rasterFlammable,n=2) <- colorRampPalette(c("blue", "red"))(2) 
-  }
-  #doing this here ensures non flammabe cells are accounted for, 
-  #no matter when/where rasterBurnProb and rasterFlammable are created.
-  sim$rasterBurnProb[which(sim$rasterFlammable == 0)] <- 0 #this could turn some NAs to 0s.
-  sim$rasterCurrentBurn <- sim$rasterBurnProb * 0 #this conserves NAs
-  
-  if (params(sim)$fireNull$doAgeMapping == TRUE){
+  browser()
+  sim$rstCurrentBurn <- sim$rstBurnProb * 0 #this conserves NAs
+  setColors(sim$rstCurrentBurn,n=2) <- colorRampPalette(c("grey90", "red"))(2)
+ 
+   if (params(sim)$fireNull$doAgeMapping == TRUE){
     #ideally, we would have loaded an actual initial age map based e.g.
     #on inventory and or the national product that BEACONs uses 
     #(see the BEACONs modules for details and source)
-    sim$rasterTimeSinceFire <- sim$rasterBurnProb * 0 #this conserves NAs
+    #sim$rasterTimeSinceFire <- sim$rasterBurnProb * 0 #this conserves NAs
+    tsf <- sim$shpStudyRegion$fireReturnInterval
+    sim$rstTimeSinceFire <- rasterize(sim$shpStudyRegion,sim$LCC05,field=tsf, mask=TRUE)
+    sim$rstTimeSinceFire[which(sim$rstFlammable[] == 1)] <- NA #non-flammable areas are permanent.
     #assign legend and colours if you are serious
   }
   
-  
   #for any stats, we need to caculate how many burnable cells there are
-  N<- sum(!is.na(sim$rasterBurnProb)) 
-  N<- N - which(sim$rasterBurnProb == 0) # we will "mask" the lakes etc. with 0, not NA
+  N<- sum(!is.na(sim$rstBurnProb)) 
+  N<- N - which(sim$rstBurnProb[] == 1) # we will "mask" the lakes etc. with 0, not NA
   sim$nBurnableCells <- N
   sim$burnLoci <- vector("numeric")
   ##
-  
-  
   sim$fireNullStats<-list(N=numeric(0),rate=numeric(0))
   
   return(invisible(sim))
@@ -159,7 +139,7 @@ fireNullPlot <- function(sim) {
 
 fireNullBurn <- function(sim) {
   # ! ----- EDIT BELOW ----- ! #
-  
+  browser()
   N<-prod(dim(sim$rasterBurnProb))
   sim$rasterCurrentBurn<-sim$rasterCurrentBurn*0 #zero, but preserve NAs
   sim$burnLoci<-which(runif(N) < sim$rasterBurnProb) #this ignores any NAs in the map.
@@ -172,23 +152,18 @@ fireNullStatsF<-function(sim){
   N<- sim$nBurnableCells
   
   sim$fireNullStats$rate<-c(sim$fireNullStats$rate,length(sim$burnLoci)/N)
-  #sim$fireNullStats$p<-c(sim$fireNullStats$p,params(sim)$fireNull$pBurn)
   sim$fireNullStats$N<-c(sim$fireNullStats$N,length(sim$burnLoci))
   return(invisible(sim))
 }
 
 .init = function(sim) {
   
-  if (!exists(shapeFileFireRegime,where=envir(sim)) ||
-      TRUE # test if it is a proper shapefile
-      ){ 
-    stop("missing or invalid shapefile: how did you even get here?")
-  }
-  
-  
- 
+  #if (!exists("shpStudyRegion",where=envir(sim)) ||
+  #    TRUE # test if it is a proper shapefile
+  #    ){ 
+  #  stop("missing or invalid shapefile: how did you even get here?")
+  #}
   
   return(invisible(sim))
 }
-### add additional events as needed by copy/pasting from above
 
---
+++ initBaseMaps/initBaseMaps.R
@@ -0,0 +1,77 @@
+defineModule(sim, list(
+  name = "initBaseMaps",
+  description = "load, reproject and crop all initial maps and shapefile required to test LandWEB models.",
+  keywords = c("testing", "map layer initialisation"),
+  authors = c(person(c("Steve", "G"), "Cumming", email="stevec@sbf.ulaval.ca", role=c("aut"))),
+  childModules = character(),
+  version = numeric_version("1.2.0.9005"),
+  spatialExtent = raster::extent(rep(NA_real_, 4)),
+  timeframe = as.POSIXlt(c(NA, NA)),
+  timeunit =  "year", #no relevence. An init module only.
+  citation = list(""),
+  documentation = list("README.txt", "initBaseMaps.Rmd"),
+  reqdPkgs = list("raster"),
+  parameters = rbind(
+    #defineParameter("paramName", "paramClass", value, min, max, "parameter description")),
+    defineParameter(".plotInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first plot event should occur"),
+    defineParameter(".plotInterval", "numeric", NA, NA, NA, "This describes the simulation time at which the first plot event should occur"),
+    defineParameter(".saveInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first save event should occur"),
+    defineParameter(".saveInterval", "numeric", NA, NA, NA, "This describes the simulation time at which the first save event should occur")
+  ),
+  inputObjects = data.frame(
+    objectName = c("LCC05X", "shpStudyRegionX"),
+    objectClass = c("RasterLayer","SpatialPolygonsDataFrame"),
+    sourceURL = "",
+    other = NA_character_,
+    stringsAsFactors = FALSE
+  ),
+  outputObjects = data.frame(
+    objectName = c("LCC05", "shpStudyRegion"),
+    objectClass = c("RasterLayer","SpatialPolygonsDataFrame"),
+    other = NA_character_,
+    stringsAsFactors = FALSE
+  )
+))
+
+## event types
+#   - type `init` is required for initialiazation
+
+doEvent.initBaseMaps = function(sim, eventTime, eventType, debug = FALSE) {
+  if (eventType == "init") {
+    ### check for more detailed object dependencies:
+    ### (use `checkObject` or similar)
+
+    # do stuff for this event
+    #sim <- initBaseMapCache(sim)
+    sim <- sim$initBaseMapsInit(sim)
+  } 
+  else {
+    warning(paste("Undefined event type: '", current(sim)[1, "eventType", with = FALSE],
+                  "' in module '", current(sim)[1, "moduleName", with = FALSE], "'", sep = ""))
+  }
+  return(invisible(sim))
+}
+
+### template initialization
+initBaseMapsInit <- function(sim) {
+ # 
+  browser()
+  simProjection <- crs(sim$LCC05X)
+  #reproject sim$shpStudyRegion to accord with LCC05
+  sim$shpStudyRegion <- spTransform(sim$shpStudyRegionX, CRSobj=simProjection)
+  sim$LCC05 <- crop(sim$LCC05X,sim$shpStudyRegion)
+  rm(LCC05X,envir=envir(sim))
+  sim$LCC05[]<-sim$LCC05[] #this kludge has the effect of forcing hthe raster in memory.
+  crs(sim$LCC05) <- simProjection #somebody once thought that crop does not preserve projections
+                                  #so we are blindly propagating this code.  
+  tmp<-getColors(sim$lcc05)[[1]]
+  sim$LCC05 <- mask(sim$LCC05,sim$shpStudyRegion)
+  setColors(sim$LCC05, n = 256) <-  tmp #mask removes colors!
+  return(invisible(sim))
+}
+
+
+initBaseMapsCache <- function(sim){
+  #
+  return(invisible(sim))
+}
