52bc85614cb4a82a140e8c7df42e83a15b67075b
emcintir <eliotmcintire@gmail.com>
2017-02-10 09:31:34 -0800

    [LandMine] add spreadProb to burning -- avoid water/rocks

--- LandMine/LandMine.R
+++ LandMine/LandMine.R
@@ -13,20 +13,20 @@ defineModule(sim, list(
   timeunit = "year",
   citation = list("citation.bib"),
   documentation = list("README.txt", "LandMine.Rmd"),
-  reqdPkgs = list("raster", "RColorBrewer", "data.table", "VGAM", "magrittr"),
+  reqdPkgs = list("raster", "RColorBrewer", "data.table", "VGAM", "magrittr", "grDevices"),
   parameters = rbind(
     #defineParameter("paramName", "paramClass", value, min, max, "parameter description")),
     defineParameter("fireTimestep", "numeric", 1, NA, NA, "This describes the simulation time at which the first plot event should occur"),
     defineParameter("burnInitialTime", "numeric", sim$startPlus1(sim), NA, NA, "This describes the simulation time at which the first plot event should occur"),
     defineParameter("flushCachedRandomFRI", "logical", FALSE, NA, NA, "If no Fire Return Interval map is supplied, then a random one will be created and cached. Use this to make a new one."),
-    defineParameter(".plotInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first plot event should occur"),
-    defineParameter(".plotInterval", "numeric", NA, NA, NA, "This describes the simulation time interval between plot events"),
+    defineParameter(".plotInitialTime", "numeric", sim$startPlus1(sim), NA, NA, "This describes the simulation time at which the first plot event should occur"),
+    defineParameter(".plotInterval", "numeric", 1, NA, NA, "This describes the simulation time interval between plot events"),
     defineParameter(".saveInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first save event should occur"),
     defineParameter(".saveInterval", "numeric", NA, NA, NA, "This describes the simulation time interval between save events"),
     defineParameter(".useCache", "numeric", FALSE, NA, NA, "Should this entire module be run with caching activated? This is generally intended for data-type modules, where stochasticity and time are not relevant")
   ),
   inputObjects = data.frame(
-    objectName = c("rstStudyRegion"),
+    objectName = c("rstStudyRegion", "rstFlammable", "fireReturnInterval"),
     objectClass = c("Raster"),
     sourceURL = "",
     other = NA_character_,
@@ -40,6 +40,9 @@ defineModule(sim, list(
                   ),
     createsOutput("fireTimestep", "numeric", 
       "The number of time units between successive fire events in a fire module"
+    ),
+    createsOutput("rstFlammableNum", "RasterLayer", paste(
+      "A binary, numeric raster indicating NA or 0 for not burnable")
     ),
     createsOutput("numFiresPerYear", "numeric", paste(
       "The average number of fires per year, by fire return interval level on rstCurrentBurn")
@@ -66,7 +69,9 @@ doEvent.LandMine = function(sim, eventTime, eventType, debug = FALSE) {
   } else if (eventType == "plot") {
     # ! ----- EDIT BELOW ----- ! #
     # do stuff for this event
-
+    sim <- LandMinePlot(sim)
+    sim <- scheduleEvent(sim, P(sim)$.plotInterval, "LandMine", "plot")
+    
     #Plot(objectFromModule) # uncomment this, replace with object to plot
     # schedule future event(s)
 
@@ -147,6 +152,9 @@ LandMineInit <- function(sim) {
   sim$fireReturnInterval[] <- as.numeric(as.character(vals))
   
   sim$rstCurrentBurn <- raster(sim$fireReturnInterval)
+  sim$rstFlammableNum <- raster(sim$rstFlammable)
+  sim$rstFlammableNum[] <- sim$rstFlammable[]
+  sim$rstFlammableNum[is.na(sim$rstFlammableNum)] <- 0
   
   
   
@@ -173,6 +181,9 @@ LandMinePlot <- function(sim) {
   # ! ----- EDIT BELOW ----- ! #
   # do stuff for this event
   #Plot("object")
+  Plot(sim$rstCurrentBurn,new=time(sim)==P(sim)$.plotInitialTime, 
+       title = "Cumulative Fire Map",
+       cols = c("transparent", adjustcolor("red", alpha.f=0.1)))
 
   # ! ----- STOP EDITING ----- ! #
   return(invisible(sim))
@@ -190,19 +201,20 @@ LandMineBurn <- function(sim) {
   
   sim$startCells <- data.table(pixel=1:ncell(sim@.envir$rstStudyRegion),
                                 fri=sim@.envir$fireReturnInterval[],key="fri") %>%
+                       na.omit() %>%
                        .[,SpaDES:::resample(pixel,numFires[.GRP]),by=fri] %>% 
                        .$V1
   fireSizes <- pmax(1, rtruncpareto(length(sim$startCells), 1, 1e4, 0.4))
   #fireSizes <- pmax(1,rexp(length(sim$startCells), rate = 1/sim@.envir$avgFireSize))
   fires <- sim$burn(sim$fireReturnInterval, startCells = sim$startCells, 
-                    fireSizes = fireSizes)
-  if(any(fires[,.N,by=id]$N < floor(fireSizes))) stop("Fire weren't exact")
+                    fireSizes = fireSizes, spreadProb = sim$rstFlammableNum)
+  
+  #if(any(fires[,.N,by=id]$N < floor(fireSizes))) stop("Fire weren't exact")
+  #browser()
+  a <- 1
+  
   sim$rstCurrentBurn[] <- 0
   sim$rstCurrentBurn[fires$indices] <- 1 # time(sim)+1
-  if(!is.na(P(sim)$.plotInitialTime)) {
-    Plot(sim$rstCurrentBurn,new=time(sim)==0, cols = c("red", "yellow"),
-         zero.color = "transparent", legendRange = c(0, end(sim)))
-  }
   #Plot(hist(fires[,.N,by=id]$N), title = "fire size distribution")
   
 
