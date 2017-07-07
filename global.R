275421e0be6a6744c0efe285062195f9db66bbcf
Eliot McIntire <eliotmcintire@gmail.com>
2017-07-06 21:09:25 -0400

    global

--- global.R
+++ global.R
@@ -45,9 +45,9 @@ machines <- c("localhost" = maxNumClusters) #, "132.156.148.91"=5, "132.156.149.
 # Time steps
 fireTimestep <- 1
 successionTimestep <- 10 # was 2
-endTime <- 100 # was 4
+endTime <- 20 # was 4
 summaryInterval <- 10#endTime/2 # was 2
-summaryPeriod <- c(50, endTime)
+summaryPeriod <- c(10, endTime)
 
 # Spatial stuff
 #studyArea <- "FULL"
