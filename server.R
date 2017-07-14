f6a97cbe6c33b74289fe4971aacfc6cb901437ca
Eliot McIntire <eliotmcintire@gmail.com>
2017-07-14 09:38:12 -0700

    automated push post run

--- server.R
+++ server.R
@@ -356,8 +356,6 @@ function(input, output, session) {
   if(Sys.info()["nodename"]=="W-VIC-A105388") {
     Cache(workingShas, cacheRepo = reproducibleCache, notOlderThan = Sys.time(), 
           userTags = "workingShas")
-    system("git commit -a -m 'automated push post run'")
-    system("git push")
     if(usingPreviousVersion) {
       system("git checkout .")
       system("git checkout development")
