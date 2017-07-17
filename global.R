a9606d09d74c012da75e30ccbcb950ae224e0c87
Eliot McIntire <eliotmcintire@gmail.com>
2017-07-17 16:49:52 -0700

    automated push post run

--- global.R
+++ global.R
@@ -47,8 +47,8 @@ if(.reloadPreviousWorking==1) {
 studyArea <- "EXTRALARGE"
 studyArea <- "LARGE"
 #studyArea <- "MEDIUM"
-studyArea <- "SMALL"
-#studyArea <- "NWT"
+#studyArea <- "SMALL"
+studyArea <- "NWT"
 
 ## Create mySim
 paths <- list(
@@ -112,9 +112,9 @@ machines <- c("localhost" = maxNumClusters) #, "132.156.148.91"=5, "132.156.149.
 # Time steps
 fireTimestep <- 1
 successionTimestep <- 10 # was 2
-endTime <- 5 # was 4
-summaryInterval <- 3#endTime/2 # was 2
-summaryPeriod <- c(2, endTime)
+endTime <- 3 # was 4
+summaryInterval <- 10#endTime/2 # was 2
+summaryPeriod <- c(1, endTime)
 
 # leaflet parameters
 leafletZoomInit = 5 
