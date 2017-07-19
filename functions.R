2d5b5b70a4d83d31fa426d7e30420896f6d41f33
Eliot McIntire <eliotmcintire@gmail.com>
2017-07-19 15:58:30 -0700

    largePatchFn description

--- functions.R
+++ functions.R
@@ -152,6 +152,24 @@ cellNumbersForPolygon <- function(dummyRaster, Polygon) {
 }
 
 #' Calculate number of "large" patches in a landscape
+#' 
+#' This function does several things:
+#' 
+#' 1. Takes a time series of rasters, all saved to disk, named vegTypeMapFiles_xxx.tif, identified with outputs(mySimOut[[1]])
+#'    This raster shows, for each pixel, which species of tree is the "leading" species, i.e,. most dominant
+#' 2. For each age class in the vector ageClass (should be reactive, currently is not), derive a new raster, 
+#'       where each pixel that is not contained between ageClass[x] and ageClass[x+1] is made NA 
+#' 3. This new raster will have (currently) 4 values, 1 to 4, labelled: Pine, Deciduous, Spruce, Mixed
+#' 4. Again, for each of these rasters, one veg type at a time, set all other veg types to NA
+#' 5. Run raster::clump function to determine clump sizes, save as a data.table, 3 columns, polygonID, newRas, sizeInHa 
+#' 6. Repeat steps for each veg type, and each time stamp... making a list of lists (e.g., outer list = time, inner list = veg type)
+#' 7. Collapse this to a data.table with columns polygonID, newRas, sizeInHa, vegCover, rep (meaning time)
+#' 8. Collapse this to a data.table with 1 more column, adding ageClass
+#' 9. This data.table is then used in shiny app for histograms, currently in menu item "Large Patches". It has
+#'       4 dimensions which are placed 1 = menu for ageClass, 2 = tab for polygonID, 3 = histograms for vegCover, 
+#'       4 = content of histogram is sizeInHa
+#' 10. There is a reactive element that lets user choose to "omit" any clump that is "less than XXX hectares", set to 500 at start.
+#' 
 #' @return A matrix with counts of number of large patches
 largePatchesFn <- function(timeSinceFireFiles, vegTypeMapFiles,
                            polygonToSummarizeBy, cl, 
@@ -168,7 +186,8 @@ largePatchesFn <- function(timeSinceFireFiles, vegTypeMapFiles,
   
   rasWithNAs <- raster(raster(timeSinceFireFiles[1]))
   rasWithNAs[] <- NA
-  #polygonToSummarizeByRast <- Cache(rasterize, polygonToSummarizeBy, y = rasWithNAs, cacheRepo = paths$cachePath)
+  
+  # identify which polygon each pixel is contained within == data.table with 2 columns, cell and polygonID
   cellIDByPolygon <- Cache(cacheRepo = paths$cachePath,
                            cellNumbersForPolygon, rasWithNAs, polygonToSummarizeBy)
   
@@ -222,12 +241,15 @@ largePatchesFn <- function(timeSinceFireFiles, vegTypeMapFiles,
                     clumpedRasts
                   })))
     names(out1) <- yearNames
+    
+    # collapse to a single data.table
     outDT <- rbindlist(lapply(seq_along(out1), function(y) {
       a <- rbindlist(lapply(seq_along(out1[[y]]), function(x) out1[[y]][[x]][,vegCover:=names(out1[[y]])[x]]))
       a[,rep:=names(out1)[y]]
     }))
   }
   )
+  
   out <- setNames(out, ageClasses)
   out <- rbindlist(lapply(seq_along(out), function(z) {
     out[[z]][, ageClass:=names(out)[z]]
@@ -265,8 +287,6 @@ gdalSet <- function() {
   gdal_setInstallation()
   getOption("gdalUtils_gdalPath")
 }
-options(gdalUtils_gdalPath=Cache(gdalSet, cacheRepo = paths$cachePath))
-
 
 gdal2TilesFn <- function(r, filename, zoomRange=6:11, color_text_file = asPath(colorTableFile)) {
   filename1 <- filename(r)
