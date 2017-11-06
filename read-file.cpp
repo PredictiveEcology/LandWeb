62313e6e800058b9d1a238efb04f253b52e40773
Eliot McIntire <eliotmcintire@gmail.com>
2017-11-05 22:31:52 -0800

    package management
    move .packageVersions.txt to libPath folder

---
+++ read-file.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+CharacterVector read_file_cpp1(std::string path) {
+  std::ifstream t(path.c_str());
+  std::stringstream ss;
+  ss << t.rdbuf();
+  return ss.str();
+}
+
+// [[Rcpp::export]]
+CharacterVector read_file_cpp2(std::string path) {
+  std::ifstream in(path.c_str());
+  std::string contents;
+  in.seekg(0, std::ios::end);
+  contents.resize(in.tellg());
+  in.seekg(0, std::ios::beg);
+  in.read(&contents[0], contents.size());
+  in.close();
+  return(contents);
+}
