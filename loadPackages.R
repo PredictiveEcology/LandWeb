0f40e94ee17766ed95da21a7abac214d741446b4
Eliot McIntire <eliotmcintire@gmail.com>
2017-12-12 22:10:01 -0800

    more

--- loadPackages.R
+++ loadPackages.R
@@ -2,19 +2,23 @@
 ## This follows a reproducible work flow:
 # 1. need reproducible package 
 # 2. change .libPaths to a project specific one
+packageLibrary <- "Packages1"
+alreadyHasPackageLibrary <- grepl(packageLibrary, .libPaths())
+if(any(alreadyHasPackageLibrary)) .libPaths(.libPaths()[!alreadyHasPackageLibrary])
+#.libPaths(c(packageLibrary, .libPaths()))
 if(!require(devtools)) install.packages("devtools", dependencies = TRUE)
 library(devtools)
 install_github("PredictiveEcology/reproducible@development", 
                dependencies = TRUE, upgrade_dependencies = FALSE, local=FALSE)
 install_github("PredictiveEcology/SpaDES.core@development", 
                dependencies = TRUE, upgrade_dependencies = FALSE, local=FALSE)
-packageLibrary <- "Packages1"
 dir.create(packageLibrary)
 .libPaths(c(packageLibrary, .libPaths()))
 
 library(reproducible) # important to load the one in the libPaths -- or else there will be conflicts 
 Require(libPath = packageLibrary, 
-        c("devtools",
+        c("Rcpp", 
+          "devtools",
           "data.table",
           "raster",
           "magrittr",
@@ -39,14 +43,3 @@ Require(libPath = packageLibrary,
 
 if(FALSE) # only do this when you want a new snapshot taken of the packages installed
   pkgSnapshot(".packageVersions.txt", libPath = packageLibrary)
-
-reproducible::newLibPaths(packageLibrary)
-
-updatePkg <- function(pkg, pkgHash, repo) {
-  PkgDescr <- try(read.dcf(system.file(package = pkg, "DESCRIPTION")))
-  needPkg <- TRUE
-  if ("GithubSHA1" %in% colnames(PkgDescr)) {
-    if (grepl(paste0("^",pkgHash), PkgDescr[,"GithubSHA1"])) needPkg <- FALSE
-  }
-  if (needPkg) install_github(paste0(file.path(repo,pkg),"@", pkgHash))
-}
