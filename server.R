7b99d49a43d1b3a04681de778a5f07c10feaf773
Damian Rodziewicz <damianrodziewicz@gmail.com>
2017-12-22 14:56:01 +0100

    Remove parts of code that we can't move now.

--- server.R
+++ server.R
@@ -1,47 +1,9 @@
 function(input, output, session) {
-  sessionStartTime <- Sys.time()
-  
-  session$onSessionEnded(function() {
-    # if(reloadPreviousWorking) {
-    #   checkoutCondition <- reproducible:::checkoutVersion(
-    #     paste0("eliotmcintire/LandWeb@development"), cred = "GITHUB_PAT")
-    #   
-    #   #.libPaths(origLibPaths) # get out of checkpoint
-    # }
-    
-    if(TRUE)
-      if(Sys.info()["nodename"]=="W-VIC-A105388")  {
-        
-        if(rsyncToAWS) {
-          #keepCache(mySim, after = appStartTime)
-          system(paste0("rsync -ruv --exclude '.git' --exclude '.Rproj.user' --exclude '.checkpoint' --delete -e 'ssh -i ",
-                        path.expand('~'),
-                        "/.ssh/laptopTesting.pem' ~/Documents/GitHub/LandWeb/ emcintir@ec2-52-26-180-235.us-west-2.compute.amazonaws.com:/srv/shiny-server/Demo/"))
-        }
-      }
-    
-    if(!exists("appEndTime")) appEndTime <- Sys.time()
-    message("The app started at ", appStartTime)
-    message("The session started at ", sessionStartTime)
-    message("The session ended at ", Sys.time())
-    message("The simInit took ", format(endSimInit - startSimInit))
-    
-    message("The app took ", format(appEndTime - appStartTime))
-    message("The app started ", format(Sys.time() - appStartTime), " ago")
-  })
-  
-  # if(reloadPreviousWorking) {
-  #   keepArtifacts <<- unique(showCache(paths$cachePath, after = startCacheTime)$artifact)
-  #   archivist::addTagsRepo(keepArtifacts,
-  #                          repoDir = paths$cachePath,
-  #                          tags = paste0("LandWebVersion:", LandWebVersion))
-  # }
-  #react <- reactiveValues()
   seed <- sample(1e8,1)
   set.seed(seed)
   message("Current seed is: ", seed)
   spadesAndExperiment <- function(mySim, experimentReps) {
-    
+
     if (TRUE) {
       # # Do an initial run for each given study area so that all the data prep can be done once only
       #initialRun1 <- spades(Copy(mySim), debug = TRUE)
@@ -51,14 +13,14 @@ function(input, output, session) {
       end(mySimCopy) <- 0
       message("Running Initial spades call")
       initialRun <<- Cache(spades, sim = mySimCopy, #notOlderThan = Sys.time(),
-                           debug = "paste(Sys.time(), paste(unname(current(sim)), collapse = ' '))", 
-                           objects = "shpStudyRegion", 
-                           #cacheRepo = cachePath(mySim), 
-                           .plotInitialTime = NA, 
+                           debug = "paste(Sys.time(), paste(unname(current(sim)), collapse = ' '))",
+                           objects = "shpStudyRegion",
+                           #cacheRepo = cachePath(mySim),
+                           .plotInitialTime = NA,
                            omitArgs = c("debug", ".plotInitialTime"),
                            debugCache="complete")
     }
-    
+
     ##########
     raster::endCluster()
     seed <- sample(1e8,1)
@@ -68,46 +30,44 @@ function(input, output, session) {
     #startTime <<- st <<- Sys.time()
     message("Running Experiment, starting at time: ", appStartTime)
     objectsToHash <- grep("useParallel", ls(mySim@.envir, all.names = TRUE), value=TRUE, invert=TRUE)
-    args <- list(experiment, mySim, replicates = experimentReps, 
+    args <- list(experiment, mySim, replicates = experimentReps,
                  objects = objectsToHash,
-                 debug = "paste(Sys.time(), format(Sys.time() - appStartTime, digits = 2), 
+                 debug = "paste(Sys.time(), format(Sys.time() - appStartTime, digits = 2),
                  paste(unname(current(sim)), collapse = ' '))",#,
                  .plotInitialTime = NA,
                  clearSimEnv = TRUE,
-                 debugCache="complete", 
+                 debugCache="complete",
                  omitArgs = c("debug", ".plotInitialTime"))
     args <- args[!unlist(lapply(args, is.null))]
     mySimOut <- do.call(Cache, args)
     message(attr(mySimOut, "tags"))
     mySimOut
   }
-  
+
   objectsToHash <- grep("useParallel", ls(mySim@.envir, all.names=TRUE), value=TRUE, invert=TRUE)
-  
-  
+
+
   # THIS IS THE MAIN "SIMULATION FUNCTION"
-  # THE FOLLOWING OBJECT IS A LIST OF 1 simList, 
+  # THE FOLLOWING OBJECT IS A LIST OF 1 simList,
   # A simList is a rich data structure that comes with the SpaDES.core package
-  mySimOut <<- Cache(spadesAndExperiment, mySim, experimentReps, 
-                     debugCache = "complete", 
+  mySimOut <<- Cache(spadesAndExperiment, mySim, experimentReps,
+                     debugCache = "complete",
                      objects = objectsToHash)#,
                      #sideEffect = TRUE)
 
-  callModule(simInfo, "simInfoTabs", mySimOut[[1]])
-  callModule(moduleInfo, "modInfoBoxes", mySimOut[[1]])
   message("  Finished Experiment")
 
-  
-  message("  Identify which files were created during simulation")  
+
+  message("  Identify which files were created during simulation")
   # outputs() function reports on any files that were created during the simulation
   filesFromOutputs <- lapply(seq_along(mySimOut), function(x) {
     outputs(mySimOut[[x]])$file
   })
-  
+
   for(simNum in seq_along(mySimOut)) {
-    mySimOut[[simNum]]@outputs$file <- 
-      
-      lapply(strsplit(outputs(mySimOut[[simNum]])$file, 
+    mySimOut[[simNum]]@outputs$file <-
+
+      lapply(strsplit(outputs(mySimOut[[simNum]])$file,
                       split = paste0(outputPath(mySimOut[[simNum]]),"[\\/]+")), function(f) {
         f[[2]]
       }) %>%
@@ -115,11 +75,11 @@ function(input, output, session) {
       file.path(paths$outputPath, .)
   }
 
-  message("  Load rasters from disk, reproject them to leaflet projection")  
+  message("  Load rasters from disk, reproject them to leaflet projection")
   rastersFromOutputs <- lapply(seq_along(mySimOut), function(x) {
     grep(pattern = ".grd$|.tif$", outputs(mySimOut[[x]])$file, value = TRUE)
   }) %>% unlist()
-  
+
   # Look for all files named rstTimeSinceFire -- these are several rasters each with a filename
   #   that represents the simulation "time" when it was created, e.g., 10, 20, 30 years
   tsf <- grep(pattern = "rstTimeSinceFire", rastersFromOutputs, value = TRUE)
@@ -128,17 +88,17 @@ function(input, output, session) {
   vtm <- grep(pattern = "vegTypeMap", rastersFromOutputs, value = TRUE)
   lenTSF <- length(tsf)
   rasterResolution <<- raster(tsf[1]) %>% res()
-  
+
   lfltFN <- gsub(tsf, pattern = ".grd$|.tif$", replacement = "LFLT.tif")
-  
+
   globalRasters <<- Cache(reprojectRasts, lapply(tsf, asPath), digestPathContent = .quickCheck,
                           lfltFN, sp::CRS(lflt), end(mySim), cacheRepo = paths$cachePath,
                           flammableFile = asPath(file.path(paths$outputPath, "rstFlammable.grd")))
-  
+
   message("  Determine leading species by age class, by polygon (loading 2 rasters, summarize by polygon)")
-  args <- list(leadingByStage, tsf, vtm, 
+  args <- list(leadingByStage, tsf, vtm,
                polygonToSummarizeBy = ecodistricts,
-               cl = if (exists("cl")) cl, 
+               cl = if (exists("cl")) cl,
                omitArgs = "cl", digestPathContent = .quickCheck,
                ageClasses = ageClasses, cacheRepo = paths$cachePath)
   args <- args[!unlist(lapply(args, is.null))]
@@ -152,78 +112,15 @@ function(input, output, session) {
   vegLeadingTypes <- c(unique(leading$vegType))
   vegLeadingTypesWithAllSpecies <- c(vegLeadingTypes, "All species")
 
-  # Shiny reactive element  
+  # Shiny reactive element
   # Large patch size section, i.e., clumps -- reactive to text box of minimum clump size
-  observe({
-    lapply(ageClasses, function(ageClass) {
-      output[[paste0("Clumps",ageClass,"UI")]] <- renderUI({
-        ns <- session$ns
-        ageClassIndex <- pmatch(ageClass, ageClasses)
-        polygonIDs <- seq_along(ecodistricts)[polygonsWithData[ageClass==ageClasses[ageClassIndex]]$V1]
-        
-        myTabs <- lapply(polygonIDs, function(polygonID) {
-          tabPanel(paste(availablePolygonAdjective,ecodistricts$ECODISTRIC[polygonID]), 
-                   tabName = paste0("Poly",ageClass,polygonID),
-                   fluidRow(
-                     lapply(seq_along(vegLeadingTypesWithAllSpecies), function(k) {
-                       clumpModOutput(paste0(ageClassIndex, "_", polygonID, "_", k, "_clumps"),
-                                      vegLeadingTypes = vegLeadingTypesWithAllSpecies)
-                     })
-                   )
-          )               
-        })
-        fluidPage(
-          column(width = 12, h2("NRV of number of 'large' (",strong(ClumpsReturn()$patchSize," hectares"),"), ",
-                                strong(tolower(ageClasses[ageClassIndex]))," patches")),
-          column(width = 12, h4("These figures show the NRV of the probability distribution",
-                                "of ",tolower(ageClasses[ageClassIndex])," patches that are",ClumpsReturn()$patchSize," hectares",
-                                "or larger, for each given combination of Age Class, ",
-                                "Leading Vegetation, and Polygon.",
-                                "To change the patch size that defines these, type a new value",
-                                "in the menu at the left.")
-          ),
-          do.call(tabsetPanel, myTabs)
-        )
-      })
-    })
-  })
-  
-  message("  Histograms of age classes, by vegetation type (loading 2 rasters, summarize by polygon)")
-  observe({
-    lapply(ageClasses, function(ageClass) {
-      output[[paste0(ageClass,"UI")]] <- renderUI({
-        ageClassIndex <- pmatch(ageClass, ageClasses)
-        polygonIDs <- seq_along(ecodistricts)[polygonsWithData[ageClass==ageClasses[ageClassIndex]]$V1]
-        
-        myTabs <- lapply(polygonIDs, function(polygonID) {
-          tabPanel(paste(availablePolygonAdjective,ecodistricts$ECODISTRIC[polygonID]), 
-                   tabName = paste0("Poly",ageClass,polygonID),
-                   fluidRow(
-                     lapply(seq_along(vegLeadingTypes), function(k) {
-                       vegAgeModUI(paste0(ageClassIndex, "_", polygonID, "_", k),
-                                   vegLeadingTypes = vegLeadingTypes)
-                     })
-                   )
-          )               
-        })
-        fluidPage(
-          column(width = 12, h2("NRV of ",strong(tolower(ageClasses[ageClassIndex])), "(",ageClassZones[ageClassIndex],"years )",
-                                " forest, by leading vegetation")),
-          column(width = 12, h4("These figures show the NRV of the proportion of",strong(tolower(ageClasses[ageClassIndex])),
-                                "forests each polygon that are in each leading vegetation type, as labeled in the plots.",
-                                "The proportions are proportions",em("within"),"age class. In any given",
-                                "replicate, the numbers below sum to 1."
-          )),
-          do.call(tabsetPanel, myTabs)
-        )
-      })
-    })
-  })
-  
-  
+
   currentPolygon <- callModule(leafletMap, "leafletMap")
   # returns a number 1 or 2 ... up to number of unique polygons in dataset
-  
+
+  callModule(simInfo, "simInfoTabs", mySimOut[[1]])
+  callModule(moduleInfo, "modInfoBoxes", mySimOut[[1]])
+
   output$timeSinceFireUI <- renderUI({
     tabBox(width = 12,
            tabPanel("Time Since Fire maps", tabName = "timeSinceFireTab",
@@ -231,7 +128,7 @@ function(input, output, session) {
            )
     )
   })
-  
+
   output$studyRegionUI <- renderUI({
     tabBox(width = 12,
            tabPanel("Time Since Fire maps", tabName = "timeSinceFireTab",
@@ -239,7 +136,7 @@ function(input, output, session) {
            )
     )
   })
-  
+
   callModule(timeSinceFire, "timeSinceFire", rasters = globalRasters, polygonsList = polygons,
              shpStudyRegionFull, colorTableFile, timeSinceFirePalette, maxAge)
 
@@ -263,67 +160,23 @@ function(input, output, session) {
                    ageClasses = ageClasses,
                    vegLeadingTypes = vegLeadingTypesWithAllSpecies,
                    numReps = lenTSF
-        )  
+        )
       })
     })
   })
-  
+
   lapply(seq_along(ageClasses), function(ageClassIndex) {
     #  ageClassIndex <- match(ages, seq_along(ageClasses))
     lapply(polygonsWithData[ageClass==ageClasses[ageClassIndex]]$V1, function(j) {
       lapply(seq_along(vegLeadingTypes), function(k) {
-        callModule(vegAgeMod, paste0(ageClassIndex, "_", j, "_", k), 
-                   listOfProportions = leading[ageClass==ageClasses[ageClassIndex] & 
-                                                 polygonNum==j & 
+        callModule(vegAgeMod, paste0(ageClassIndex, "_", j, "_", k),
+                   listOfProportions = leading[ageClass==ageClasses[ageClassIndex] &
+                                                 polygonNum==j &
                                                  vegType==vegLeadingTypes[k]]$proportion
         )
       })
     })
   })
-  
-  
-  # There is double code here, allowing for ggplotly version (currently commented out) and ggvis version
-  #output$StudyRegion <- renderPlot(
-  if(FALSE) {
-    bind_shiny(plot_id = "StudyRegion",
-               #height = 525, 
-               #width = 800,
-               #  {
-               #ggplotly(ggStudyRegion )
-               ggStudyRegion 
-               
-               #   g[[jj]]  <- ggplot() +
-               #     geom_raster(data=packSaturationMapPts[[jj]],
-               #                 aes(long, lat, fill=Probability, text=CountryName),
-               #                 alpha = 0.98) +
-               #     geom_polygon(data=shape.fort, 
-               #                  aes(long, lat, group=group), 
-               #                  colour='black',fill=NA) +
-               #     coord_equal() +
-               #     scale_fill_gradientn(colors=c('blue','blue','light blue', 'light blue','orange','red'),
-               #                          #scale_fill_gradientn(colors=c('#ffffd4','#fee391','#fec44f','#fe9929','#d95f0e','#993404'),
-               #                          name='Probability of a pack',  na.value = NA) + 
-               #     theme(
-               #       legend.position = 'right',
-               #       legend.key.size = unit(1, "cm")
-               #     ) +
-               #     scale_x_continuous(expand=c(0,0)) + 
-               #     scale_y_continuous(expand=c(0,0)) +
-               #     labs(x='Easting', y='Northing', title=paste("Probability of pack presence in 2097, based on",
-               #                                                 length(numPacks2AByTime[[jj]]), "replicate simulations, \n",
-               #                                                 "with", jj, "starting locations"))
-               #   
-               #   #g1 <- g
-               #}
-               # gridExtra::grid.arrange(grobs = g)
-               #ggplotly(g, tooltip = c("CountryName", "Probability"))
-               
-               #clearPlot()
-               #Plot(shpStudyRegionOrig, title = "Proportion of leading spruce")
-               #Plot(shpStudyRegion, addTo = "shpStudyRegionOrig")
-               #}
-    )
-  }
 
   clumpMod2Args <- list(
     currentPolygon = polygons[[1 + length(polygons)/4]],
@@ -335,36 +188,15 @@ function(input, output, session) {
 
   callModule(largePatches, "largePatches", numberOfSimulationTimes = lenTSF, clumpMod2Args)
 
-  
+
   output$speciesInputs <- renderDataTable({
     landisInputs
   })#, digits = 1)
-  
+
   output$speciesEcoregionInputs <- renderDataTable({
     spEcoReg
   })#, digits = 1)
 
-
-  if(Sys.info()["nodename"]=="W-VIC-A105388") {
-    if(.reloadPreviousWorking>0) { # working on temporary head
-      system("git checkout .") # delete any changes
-      system("git checkout development") # go back to development
-      system("git stash pop") # pop the stashed anything
-      rm(.reloadPreviousWorking, envir=.GlobalEnv)
-    } else { # working on development -- add and push so that it stores a copy of a completed version
-      system("git add .")
-      system("git commit -a -m 'automated push post run'")
-      system("git push")
-    }
-    Cache(workingShas, cacheRepo = reproducibleCache, date = Sys.time(), 
-          userTags = c("workingShas", paste0("endTime:",end(mySim)), paste0("scale:", studyArea)))
-  }
-    # if(Sys.info()["nodename"]=="W-VIC-A105388") {
-  #   system("git commit -a -m 'pre run'")
-  #   system("git push")
-  # }
   noLongerWaiting()
   message("  Finished global.R")
-  
-  appEndTime <- Sys.time()  
 }
