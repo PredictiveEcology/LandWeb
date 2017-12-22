482cb45ec1914747919f2f47f225790f190f154c
Damian Rodziewicz <damianrodziewicz@gmail.com>
2017-12-22 15:33:39 +0100

    Remove from UI parts that we don't generate.

--- server.R
+++ server.R
@@ -23,44 +23,6 @@ function(input, output, session) {
   callModule(timeSinceFire, "timeSinceFire", rasters = globalRasters, polygonsList = polygons,
              shpStudyRegionFull, colorTableFile, timeSinceFirePalette, maxAge)
 
-  args <- list(clumpMod2, "id1", session = session,
-               currentPolygon = polygons[[1 + length(polygons)/4]],
-               tsf = tsf, vtm = vtm,
-               cl = if(exists("cl")) cl,
-               ageClasses = ageClasses, cacheRepo = paths$cachePath,
-               patchSize = reactive({input$PatchSize33}),
-               largePatchesFn = largePatchesFn)
-  args <- args[!unlist(lapply(args, is.null))]
-  ClumpsReturn <- do.call(callModule, args )
-  rm(args)
-
-  lapply(seq_along(ageClasses), function(ageClassIndex) { # ageClassIndex is age
-    lapply(polygonsWithData[ageClass==ageClasses[ageClassIndex]]$V1, function(j) { # j is polygon index
-      lapply(seq_along(vegLeadingTypesWithAllSpecies), function(k) { # k is Veg type
-        callModule(clumpMod, paste0(ageClassIndex, "_", j, "_", k, "_clumps"),
-                   Clumps = reactive({ClumpsReturn()$Clumps}),
-                   id = paste0(ageClassIndex, "_", j, "_", k, "_clumps"),
-                   ageClasses = ageClasses,
-                   vegLeadingTypes = vegLeadingTypesWithAllSpecies,
-                   numReps = lenTSF
-        )
-      })
-    })
-  })
-
-  lapply(seq_along(ageClasses), function(ageClassIndex) {
-    #  ageClassIndex <- match(ages, seq_along(ageClasses))
-    lapply(polygonsWithData[ageClass==ageClasses[ageClassIndex]]$V1, function(j) {
-      lapply(seq_along(vegLeadingTypes), function(k) {
-        callModule(vegAgeMod, paste0(ageClassIndex, "_", j, "_", k),
-                   listOfProportions = leading[ageClass==ageClasses[ageClassIndex] &
-                                                 polygonNum==j &
-                                                 vegType==vegLeadingTypes[k]]$proportion
-        )
-      })
-    })
-  })
-
   clumpMod2Args <- list(
     currentPolygon = polygons[[1 + length(polygons)/4]],
     tsf = tsf, vtm = vtm,
--- ui.R
+++ ui.R
@@ -1,32 +1,11 @@
 dashboardPage(skin = "green",
   dashboardHeader(title = "LandWeb"),
   dashboardSidebar(width = 300,
-    sidebarMenu(id = "wholeThing", 
+    sidebarMenu(id = "wholeThing",
       menuItem("Maps", icon = icon("map-o"),
                menuSubItem("Time since fire", tabName = "TimeSinceFire", icon = icon("clock-o"), selected = TRUE)),
-               #menuSubItem("Change polygon layer", tabName = "Polygons", icon = icon("map-marker"), selected = FALSE)),
-               #menuSubItem("Fire Return Interval Map", tabName = "StudyRegion",
-               #             icon = icon("map"))),
       br(),
       h4(HTML("&nbsp;"), "NRV plots"),
-      #tags$head(tags$script(HTML('$(document).ready(function() {$(".treeview-menu").css("display", "block");})'))),
-      menuItem("Large Patches", icon = icon("bar-chart"),
-               menuSubItem(#text=tags$div("Type patch size in hectares",tags$br(),
-                          #            "that defines 'Large'",tags$br(),
-                          #            "(numbers below 100 will not work)",
-                          #            style = "display: inline-block; vertical-align: middle;"), 
-                        tabName = "patchSize",
-                        #icon = icon("arrows-alt"), 
-                        clumpMod2Input("id1")),
-               menuSubItem("Young", tabName = "ClumpsYoung", icon = icon("tree"), selected = FALSE),
-               menuSubItem("Immature", tabName = "ClumpsImmature", icon = icon("tree")),
-               menuSubItem("Mature", tabName = "ClumpsMature", icon = icon("tree")),
-               menuSubItem("Old", tabName = "ClumpsOld", icon = icon("tree"), selected = FALSE)),
-      menuItem("Proportion Area in Age Class By Cover", icon = icon("bar-chart"),
-               menuSubItem("Young Forest", tabName = "Young", icon = icon("tree"), selected = FALSE),
-               menuSubItem("Immature Forest", tabName = "Immature", icon = icon("tree")),
-               menuSubItem("Mature Forest", tabName = "Mature", icon = icon("tree")),
-               menuSubItem("Old Forest", tabName = "Old", icon = icon("tree"))),
       menuItem("Large Patches - new", tabName = "largePatchesSlicer", icon = icon("bar-chart")),
       br(),
       h4(HTML("&nbsp;"), "Model details"),
@@ -40,76 +19,15 @@ dashboardPage(skin = "green",
   dashboardBody(
     includeCSS("www/style.css"),
     useShinyjs(),
-    #tags$head(tags$style(HTML(mycss))),
-    div(
-      id = "loading_page",
-      h1("Loading, please wait ..."),
-      tags$img(src = #"http://cdn.boatinternational.com/assets/spinner-2cd0a51f39b9c1d559af6c393f97a80c.gif",
-               "http://s01.europapress.net/chance/loading.gif",
-               id = "loading-spinner",
-               width = "100px", height = "100px"),
-      br(),
-      
-      style="text-align: center;"
-    ),
-    
-    #hidden(
       div(
         id = "main_content",
-    
+
         tabItems(
-          # tabItem("StudyRegion",
-          #         fluidRow(
-          #           box(
-          #             width = 12,
-          #             height = 900,
-          #             solidHeader = TRUE,
-          #             status = "success",
-          #             title = "Fire Return Interval, with sub region currently in Demo (hover mouse over for values)",
-          #             #plotOutput("StudyRegion", width = "100%")#, height = "100%")
-          #             #plotlyOutput("StudyRegion", width = "100%")#, height = "100%")
-          #             ggvisOutput("StudyRegion")
-          #           )
-          #         )
-          # ),
           tabItem("simDiagrams", simInfoUI("simInfoTabs")),
           tabItem("moduleInfo", moduleInfoUI("modInfoBoxes")),
           tabItem("largePatchesSlicer", largePatchesUI("largePatches")),
-          tabItem("ClumpsYoung",
-                  fluidRow(uiOutput("ClumpsYoungUI"))
-          ),
-          tabItem("ClumpsImmature",
-                  fluidRow(uiOutput("ClumpsImmatureUI"))
-          ),
-          tabItem("ClumpsMature",
-                  fluidRow(uiOutput("ClumpsMatureUI"))
-          ),
-          tabItem("ClumpsOld",
-                  fluidRow(uiOutput("ClumpsOldUI"))
-          ),
-          tabItem("Polygons",
-                  fluidRow(
-                    tabBox(width = 12,
-                           tabPanel("Current Polygons", tabName = "Polygons1",
-                                    fluidRow(leafletMapUI("leafletMap"))
-                           )
-                    )
-                  )
-          ),
           tabItem("TimeSinceFire",
                   fluidRow(uiOutput("timeSinceFireUI"))
-          ),
-          tabItem("Young",
-                  fluidRow(uiOutput("YoungUI"))
-          ),
-          tabItem("Immature",
-                  fluidRow(uiOutput("ImmatureUI"))
-          ),
-          tabItem("Mature",
-                  fluidRow(uiOutput("MatureUI"))
-          ),
-          tabItem("Old",
-                  fluidRow(uiOutput("OldUI"))
           ),
           tabItem("inputTables",
                   fluidRow(
@@ -131,79 +49,9 @@ dashboardPage(skin = "green",
                       dataTableOutput("speciesEcoregionInputs")
                     )
                   )
-          ),
-          tabItem("OldDecidByPoly",
-                  fluidRow(
-                    box(
-                      width = 12,
-                      height = 900,
-                      solidHeader = TRUE,
-                      status = "success",
-                      title = "Initial Conditions Gone for Natural Range of Variation Calculation",
-                      plotOutput("OldDecidByPoly")
-                    )
-                  )
-          ),
-          tabItem("seralStage1",
-                  fluidRow(
-                    box(
-                      width = 6,
-                      solidHeader = TRUE,
-                      #width = 8, 
-                      status = "success",
-                      title = "% Cover by each seral stage",
-                      plotOutput("seralStagePctCover")
-                    ),
-                    box(
-                      width = 4,
-                      solidHeader = TRUE,
-                      status = "success",
-                      title = "Seral stage for region",
-                      plotOutput("seralStagePctCoverMap")
-                    )
-                  )
-          ),
-          tabItem("vegType",
-                  fluidRow(
-                    box(
-                      width = 6,
-                      solidHeader = TRUE,
-                      status = "success",
-                      title = "% Cover by each vegatation type",
-                      plotOutput("vegTypePctCover")
-                    ),
-                    box(
-                      width = 4,
-                      solidHeader = TRUE,
-                      status = "success",
-                      title = "Vegetation types for region",
-                      plotOutput("vegTypePctCoverMap")
-                    )
-                  )
-          ),
-          tabItem("patchSizes",
-                  fluidRow(
-                    box(
-                      width = 6,
-                      solidHeader = TRUE,
-                      status = "success",
-                      title = "Number of patches, by seral stage",
-                      plotOutput("patchSizesFig5")
-                    ),
-                    box(
-                      width = 5,
-                      solidHeader = TRUE,
-                      status = "success",
-                      title = "Over old patches",
-                      plotOutput("patchSizesFig5Raster")
-                    )
-                  )
           )
         )
-    #  ) # hidden
-    ),
-    
-    copyrightFooter()
+    )
   )
 )
 
