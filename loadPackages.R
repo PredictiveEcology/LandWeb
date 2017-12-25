367d83ee9e33bf3e80e1a52b2d4affb5ecf1b94c
Eliot McIntire <eliotmcintire@gmail.com>
2017-12-25 00:52:03 -0500

    simplify loadPackages.R with new Require

--- loadPackages.R
+++ loadPackages.R
@@ -4,40 +4,37 @@ packageLibrary <- file.path(dirname(tempdir()), "Packages")
 alreadyHasPackageLibrary <- grepl(packageLibrary, .libPaths())
 if(any(alreadyHasPackageLibrary)) .libPaths(.libPaths()[!alreadyHasPackageLibrary])
 #.libPaths(c(packageLibrary, .libPaths()))
-if(!require(devtools)) install.packages("devtools", dependencies = TRUE)
-library(devtools)
-install_github("PredictiveEcology/reproducible@development", 
-               dependencies = TRUE, upgrade_dependencies = FALSE, local=FALSE)
-install_github("PredictiveEcology/SpaDES.core@development", 
-               dependencies = TRUE, upgrade_dependencies = FALSE, local=FALSE)
+#if(!require(devtools)) install.packages("devtools", dependencies = TRUE)
+#library(devtools)
+# install_github("PredictiveEcology/reproducible@development", 
+#                dependencies = TRUE, upgrade_dependencies = FALSE, local=FALSE)
+# install_github("PredictiveEcology/SpaDES.core@development", 
+#                dependencies = TRUE, upgrade_dependencies = FALSE, local=FALSE)
 suppressWarnings(dir.create(packageLibrary))
 
 library(reproducible) # important to load the one in the libPaths -- or else there will be conflicts 
 
 .libPaths(c(packageLibrary, .libPaths()))
+modulePkgs <- unique(unlist(SpaDES.core::packages(module = modules)))
 Require(libPath = packageLibrary, 
-        c("Rcpp", 
-          "devtools",
-          "data.table",
-          "raster",
-          "magrittr",
-          "rgeos",
-          "dplyr",
+        c("achubaty/amc@development", 
+          "ecohealthalliance/fasterize",
+          "PredictiveEcology/SpaDES.core@development",
+          "PredictiveEcology/SpaDES.tools@randomPolygon", 
+          
+          # modules
+          modulePkgs,
+          #"devtools",
+
+          # shiny app
           "leaflet",
           "shiny",
           "shinydashboard",
           "shinyBS",
           "shinyjs",
           "shinycssloaders",
-          "VGAM",
           if (Sys.info()["sysname"] != "Windows") "Cairo",
-          if (Sys.info()["sysname"] == "Windows") "snow",# Required internally inside "parallel" package for Windows SOCK clusters
-          "purrr",
-          "gdalUtils",
-          "achubaty/amc@development", 
-          "ecohealthalliance/fasterize",
-          "PredictiveEcology/SpaDES.core@development",
-          "PredictiveEcology/SpaDES.tools@randomPolygon"
+          if (Sys.info()["sysname"] == "Windows") "snow"# Required internally inside "parallel" package for Windows SOCK clusters
         ), packageVersionFile = file.path(packageLibrary, ".packageVersions.txt"))
 
 data.table::setDTthreads(6)
