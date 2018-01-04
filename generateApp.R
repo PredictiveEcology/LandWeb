f9888c0161e92322e920c7116cf255bdfc52ac6e
Eliot McIntire <eliotmcintire@gmail.com>
2018-01-04 11:52:31 -0800

    generateApp -- use tribble for horizontal building of app

--- generateApp.R
+++ generateApp.R
@@ -16,14 +16,42 @@ appMetadata <- list(
     stringsAsFactors = FALSE
   )
 )
-
 appMetadata$modules$parameters <- list(list("rasters = globalRasters", "polygonsList = polygons", "shpStudyRegionFull", "colorTableFile", "timeSinceFirePalette", "maxAge"),
-                                       list("numberOfSimulationTimes = lenTSF", "clumpMod2Args"), list("mySimOut[[1]]"), list("mySimOut[[1]]"), list())
+                                       list("numberOfSimulationTimes = lenTSF", "clumpMod2Args"), 
+                                       list("mySimOut[[1]]"), 
+                                       list("mySimOut[[1]]"), 
+                                       list())
 appMetadata$layout$moduleUIParameters <- list(list("length(tsf)"), list(), list(), list(), list())
 
-newApp(getwd(), appMetadata)
+# newApp(getwd(), appMetadata)
+# 
+# file.copy("global_file.R", "global.R", overwrite = TRUE)
+# setwd("~/GitHub/LandWeb/")
+# shiny::runApp(".")
+
+
+Modules <- tribble(
+  ~type,  ~name, ~id, ~parameters,
+  "shinyModule", "timeSinceFire", "timeSinceFire", list("rasters = globalRasters", "polygonsList = polygons", "shpStudyRegionFull", "colorTableFile", "timeSinceFirePalette", "maxAge"),
+  "shinyModule", "largePatches", "largePatches", list("numberOfSimulationTimes = lenTSF", "clumpMod2Args"), 
+  "shinyModule", "simInfo", "simInfo", list("mySimOut[[1]]"), 
+  "shinyModule", "moduleInfo", "moduleInfo", list("mySimOut[[1]]"), 
+  "shinyModule", "inputTables", "inputTables", list()
+)
+
+Layout <- tribble(
+  ~tabName,  ~menuItemName, ~icon, ~moduleId, ~moduleUIParameters,
+  "timeSinceFire", "Maps - time since fire", "map-o", "timeSinceFire", list("length(tsf)"),
+  "largePatches", "Large Patches", "bar-chart",  "largePatches", list(),
+  "simInfo", "Overview Diagrams", "sitemap", "simInfo", list(),
+  "moduleInfo", "Module Info", "puzzle-piece", "moduleInfo", list(),
+  "inputTables", "LBMR (Succession) Model Inputs", "table", "inputTables", list()
+)
+
+appMetadata2 <- list(modules = as.data.frame(Modules), layout = as.data.frame(Layout))
+
+newApp(getwd(), appMetadata2)
 
 file.copy("global_file.R", "global.R", overwrite = TRUE)
-
-shiny::runApp("./")
-
+setwd("~/GitHub/LandWeb/")
+shiny::runApp(".")
