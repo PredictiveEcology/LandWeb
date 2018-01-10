13bb515ac19e73784e696fe9e53b6c60bca73147
Eliot McIntire <eliotmcintire@gmail.com>
2018-01-09 20:54:13 -0800

    package loading, installing

--- loadPackages.R
+++ loadPackages.R
@@ -1,21 +1,21 @@
 if(FALSE) {
   try(detach("package:SpaDES.shiny", unload = TRUE));
-  # try(detach("package:SpaDES.addins", unload = TRUE));
-  # try(detach("package:SpaDES.tools", unload = TRUE));
+   try(detach("package:SpaDES.addins", unload = TRUE));
+   try(detach("package:SpaDES.tools", unload = TRUE));
    try(detach("package:SpaDES.core", unload = TRUE));
    try(detach("package:reproducible", unload = TRUE));
-   devtools::load_all("~/GitHub/reproducible")
-   devtools::load_all("~/GitHub/SpaDES.core")
-   devtools::load_all("~/GitHub/SpaDES.tools")
-  devtools::load_all("~/GitHub/SpaDES.shiny")
+   devtools::load_all("~/Documents/GitHub/reproducible")
+   devtools::load_all("~/Documents/GitHub/SpaDES.core")
+   #devtools::load_all("~/GitHub/SpaDES.tools")
+  #devtools::load_all("~/GitHub/SpaDES.shiny")
  }
 # Packages for global.R -- don't need to load packages for modules -- happens automatically
-library(SpaDES)
-library(SpaDES.shiny)
-library(raster) 
+library(reproducible)
 
+SpaDESPkgs <- c("SpaDES", "PredictiveEcology/SpaDES.shiny@develop", "raster")
 shinyPkgs <- c("leaflet", "shiny", "shinydashboard", "shinyBS", "shinyjs", "shinycssloaders", "gdalUtils", "rgeos")
 Require(c(
+        SpaDESPkgs,
         if (Sys.info()["sysname"] != "Windows") "Cairo",
         if (Sys.info()["sysname"] == "Windows") "snow",# Required internally inside "parallel" package for Windows SOCK clusters
         # shiny app
