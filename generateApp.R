28141e31880deec74637b52d5cfe1e9b0d515d54
Eliot McIntire <eliotmcintire@gmail.com>
2018-01-09 16:41:02 -0800

    app

--- generateApp.R
+++ generateApp.R
@@ -3,33 +3,33 @@ devtools::install_github("PredictiveEcology/reproducible", ref = "development")
 library(SpaDES.shiny)
 library(dplyr)
 
-appMetadata <- list(
-  modules = data.frame(
-    type = c("shinyModule", "shinyModule", "shinyModule", "shinyModule", "shinyModule"),
-    name = c("timeSinceFire", "largePatches", "simInfo", "moduleInfo", "inputTables"),
-    id = c("timeSinceFire", "largePatches", "simInfo", "moduleInfo", "inputTables"),
-    stringsAsFactors = FALSE
-  ),
-  layout = data.frame(
-    tabName = c("timeSinceFire", "largePatches", "simInfo", "moduleInfo", "inputTables"),
-    menuItemName = c("Maps - time since fire", "Large Patches", "Overview Diagrams", "Module Info", "LBMR (Succession) Model Inputs"),
-    icon = c("map-o", "bar-chart", "sitemap", "puzzle-piece", "table"),
-    moduleId = c("timeSinceFire", "largePatches", "simInfo", "moduleInfo", "inputTables"),
-    stringsAsFactors = FALSE
-  )
-)
-appMetadata$modules$parameters <- list(list("rasters = globalRasters", "polygonsList = polygons", "shpStudyRegionFull", "colorTableFile", "timeSinceFirePalette", "maxAge"),
-                                       list("numberOfSimulationTimes = lenTSF", "clumpMod2Args"), 
-                                       list("mySimOut[[1]]"), 
-                                       list("mySimOut[[1]]"), 
-                                       list())
-appMetadata$layout$moduleUIParameters <- list(list("length(tsf)"), list(), list(), list(), list())
-
-# newApp(getwd(), appMetadata)
+# appMetadata <- list(
+#   modules = data.frame(
+#     type = c("shinyModule", "shinyModule", "shinyModule", "shinyModule", "shinyModule"),
+#     name = c("timeSinceFire", "largePatches", "simInfo", "moduleInfo", "inputTables"),
+#     id = c("timeSinceFire", "largePatches", "simInfo", "moduleInfo", "inputTables"),
+#     stringsAsFactors = FALSE
+#   ),
+#   layout = data.frame(
+#     tabName = c("timeSinceFire", "largePatches", "simInfo", "moduleInfo", "inputTables"),
+#     menuItemName = c("Maps - time since fire", "Large Patches", "Overview Diagrams", "Module Info", "LBMR (Succession) Model Inputs"),
+#     icon = c("map-o", "bar-chart", "sitemap", "puzzle-piece", "table"),
+#     moduleId = c("timeSinceFire", "largePatches", "simInfo", "moduleInfo", "inputTables"),
+#     stringsAsFactors = FALSE
+#   )
+# )
+# appMetadata$modules$parameters <- list(list("rasters = globalRasters", "polygonsList = polygons", "shpStudyRegionFull", "colorTableFile", "timeSinceFirePalette", "maxAge"),
+#                                        list("numberOfSimulationTimes = lenTSF", "clumpMod2Args"), 
+#                                        list("mySimOut[[1]]"), 
+#                                        list("mySimOut[[1]]"), 
+#                                        list())
+# appMetadata$layout$moduleUIParameters <- list(list("length(tsf)"), list(), list(), list(), list())
 # 
-# file.copy("global_file.R", "global.R", overwrite = TRUE)
-# setwd("~/GitHub/LandWeb/")
-# shiny::runApp(".")
+# # newApp(getwd(), appMetadata)
+# # 
+# # file.copy("global_file.R", "global.R", overwrite = TRUE)
+# # setwd("~/GitHub/LandWeb/")
+# # shiny::runApp(".")
 
 
 Modules <- tribble(
