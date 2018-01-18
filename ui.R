9767061043d7d721982f0cca570c65cf90163ba2
Eliot McIntire <eliotmcintire@gmail.com>
2018-01-17 20:30:03 -0800

    ui with logos

--- ui.R
+++ ui.R
@@ -11,8 +11,17 @@ menuItem("Large Patches", tabName = "largePatches", icon = icon("bar-chart")),
 menuItem("Overview Diagrams", tabName = "simInfo", icon = icon("sitemap")),
 menuItem("Module Info", tabName = "moduleInfo", icon = icon("puzzle-piece")),
 menuItem("LBMR (Succession) Model Inputs", tabName = "inputTables", icon = icon("table"))
-                               )
-              ),
+                               ),
+                               HTML(paste(
+    "<footer>",
+    "<div id=\"sidebar\" style='position: absolute; bottom: 5px; margin: 15px'>",
+    " Built with ", # nolint
+    "<a href=\"http://SpaDES.PredictiveEcology.org\">  <img src=\"http://predictiveecology.org/img/avatar.png\", height=25px>SpaDES, </a>",
+    "<a href=\"http://shiny.rstudio.com/\", target=\"_blank\">shiny</a> and ",
+    "<a href=\"https://appsilondatascience.com/\">  <img src=\"http://d3u4jj2f3q2139.cloudfront.net/logo-appsilon-data-science-transparent.png\", height=20px> </a>",
+    "</div>",
+    "</footer>"
+  ))              ),
               dashboardBody(
                 tabItems(
                   tabItem("timeSinceFire", timeSinceFireUI("timeSinceFire", length(tsf))),
@@ -23,3 +32,4 @@ tabItem("inputTables", inputTablesUI("inputTables"))
                 )
               )
 )
+
