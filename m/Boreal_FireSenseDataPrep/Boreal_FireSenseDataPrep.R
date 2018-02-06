2d713186b6e64290c48a1919476a579b5ee62ebb
JM <jean.d.marchal@gmail.com>
2018-02-06 10:24:29 -0500

    Introduce Boreal_FireSenseDataPrep

---
+++ m/Boreal_FireSenseDataPrep/Boreal_FireSenseDataPrep.R
@@ -0,0 +1,215 @@
+
+# Everything in this file gets sourced during simInit, and all functions and objects
+# are put into the simList. To use objects, use sim$xxx, and are thus globally available
+# to all modules. Functions can be used without sim$ as they are namespaced, like functions
+# in R packages. If exact location is required, functions will be: sim$<moduleName>$FunctionName
+defineModule(sim, list(
+  name = "Boreal_FireSenseDataPrep",
+  description = "A data preparation module for running the fireSense module in the LandWeb project",
+  keywords = c("LandWeb", "fireSense"),
+  authors = c(person(c("Jean", "Marchal"), email="jean.d.marchal@gmail.com", role=c("aut", "cre"))),
+  childModules = character(0),
+  version = list(SpaDES.core = "0.1.1", Boreal_FireSenseDataPrep = "0.0.1"),
+  spatialExtent = raster::extent(rep(NA_real_, 4)),
+  timeframe = as.POSIXlt(c(NA, NA)),
+  timeunit = "year",
+  citation = list("citation.bib"),
+  documentation = list("README.txt", "Boreal_FireSenseDataPrep.Rmd"),
+  reqdPkgs = list(),
+  parameters = rbind(
+    #defineParameter("paramName", "paramClass", value, min, max, "parameter description"),
+    defineParameter(".plotInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first plot event should occur"),
+    defineParameter(".plotInterval", "numeric", NA, NA, NA, "This describes the simulation time interval between plot events"),
+    defineParameter(".saveInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first save event should occur"),
+    defineParameter(".saveInterval", "numeric", NA, NA, NA, "This describes the simulation time interval between save events"),
+    defineParameter(".useCache", "numeric", FALSE, NA, NA, "Should this entire module be run with caching activated? This is generally intended for data-type modules, where stochasticity and time are not relevant")
+  ),
+  inputObjects = bind_rows(
+    #expectsInput("objectName", "objectClass", "input object description", sourceURL, ...),
+    expectsInput(objectName = "NFDB_point", objectClass = NA,
+                 desc = "Canadian National Fire Database (CNFDB) Fire Point locations and fire attributes",
+                 sourceURL = "http://cwfis.cfs.nrcan.gc.ca/downloads/nfdb/fire_pnt/current_version/NFDB_point.zip"),
+    expectsInput(objectName = "studyArea", objectClass = "SpatialPolygonsDataFrame",
+                 desc = "This shapefile describes the outline of the study area.",
+                 sourceURL = NA)
+  ),
+  outputObjects = bind_rows(
+    #createsOutput("objectName", "objectClass", "output object description", ...),
+    createsOutput(objectName = NA, objectClass = NA, desc = NA)
+  )
+))
+
+## event types
+#   - type `init` is required for initialiazation
+
+doEvent.Boreal_FireSenseDataPrep = function(sim, eventTime, eventType, debug = FALSE) {
+  switch(
+    eventType,
+    init = {
+      ### check for more detailed object dependencies:
+      ### (use `checkObject` or similar)
+
+      # do stuff for this event
+      sim <- Init(sim)
+
+      # schedule future event(s)
+      sim <- scheduleEvent(sim, P(sim)$.plotInitialTime, "Boreal_FireSenseDataPrep", "plot")
+      sim <- scheduleEvent(sim, P(sim)$.saveInitialTime, "Boreal_FireSenseDataPrep", "save")
+    },
+    plot = {
+      # ! ----- EDIT BELOW ----- ! #
+      # do stuff for this event
+
+      #Plot(objectFromModule) # uncomment this, replace with object to plot
+      # schedule future event(s)
+
+      # e.g.,
+      #sim <- scheduleEvent(sim, time(sim) + P(sim)$.plotInterval, "Boreal_FireSenseDataPrep", "plot")
+
+      # ! ----- STOP EDITING ----- ! #
+    },
+    save = {
+      # ! ----- EDIT BELOW ----- ! #
+      # do stuff for this event
+
+      # e.g., call your custom functions/methods here
+      # you can define your own methods below this `doEvent` function
+
+      # schedule future event(s)
+
+      # e.g.,
+      # sim <- scheduleEvent(sim, time(sim) + P(sim)$.saveInterval, "Boreal_FireSenseDataPrep", "save")
+
+      # ! ----- STOP EDITING ----- ! #
+    },
+    event1 = {
+      # ! ----- EDIT BELOW ----- ! #
+      # do stuff for this event
+
+      # e.g., call your custom functions/methods here
+      # you can define your own methods below this `doEvent` function
+
+      # schedule future event(s)
+
+      # e.g.,
+      # sim <- scheduleEvent(sim, time(sim) + increment, "Boreal_FireSenseDataPrep", "templateEvent")
+
+      # ! ----- STOP EDITING ----- ! #
+    },
+    event2 = {
+      # ! ----- EDIT BELOW ----- ! #
+      # do stuff for this event
+
+      # e.g., call your custom functions/methods here
+      # you can define your own methods below this `doEvent` function
+
+      # schedule future event(s)
+
+      # e.g.,
+      # sim <- scheduleEvent(sim, time(sim) + increment, "Boreal_FireSenseDataPrep", "templateEvent")
+
+      # ! ----- STOP EDITING ----- ! #
+    },
+    warning(paste("Undefined event type: '", current(sim)[1, "eventType", with = FALSE],
+                  "' in module '", current(sim)[1, "moduleName", with = FALSE], "'", sep = ""))
+  )
+  return(invisible(sim))
+}
+
+## event functions
+#   - follow the naming convention `modulenameEventtype()`;
+#   - `modulenameInit()` function is required for initiliazation;
+#   - keep event functions short and clean, modularize by calling subroutines from section below.
+
+### template initialization
+Init <- function(sim) {
+  # # ! ----- EDIT BELOW ----- ! #
+
+  # ! ----- STOP EDITING ----- ! #
+
+  return(invisible(sim))
+}
+
+### template for save events
+Save <- function(sim) {
+  # ! ----- EDIT BELOW ----- ! #
+  # do stuff for this event
+  sim <- saveFiles(sim)
+
+  # ! ----- STOP EDITING ----- ! #
+  return(invisible(sim))
+}
+
+### template for plot events
+Plot <- function(sim) {
+  # ! ----- EDIT BELOW ----- ! #
+  # do stuff for this event
+  #Plot("object")
+
+  # ! ----- STOP EDITING ----- ! #
+  return(invisible(sim))
+}
+
+### template for your event1
+Event1 <- function(sim) {
+  # ! ----- EDIT BELOW ----- ! #
+  # THE NEXT TWO LINES ARE FOR DUMMY UNIT TESTS; CHANGE OR DELETE THEM.
+  sim$event1Test1 <- " this is test for event 1. " # for dummy unit test
+  sim$event1Test2 <- 999 # for dummy unit test
+
+
+  # ! ----- STOP EDITING ----- ! #
+  return(invisible(sim))
+}
+
+### template for your event2
+Event2 <- function(sim) {
+  # ! ----- EDIT BELOW ----- ! #
+  # THE NEXT TWO LINES ARE FOR DUMMY UNIT TESTS; CHANGE OR DELETE THEM.
+  sim$event2Test1 <- " this is test for event 2. " # for dummy unit test
+  sim$event2Test2 <- 777  # for dummy unit test
+
+
+  # ! ----- STOP EDITING ----- ! #
+  return(invisible(sim))
+}
+
+.inputObjects <- function(sim) {
+  # Any code written here will be run during the simInit for the purpose of creating
+  # any objects required by this module and identified in the inputObjects element of defineModule.
+  # This is useful if there is something required before simulation to produce the module
+  # object dependencies, including such things as downloading default datasets, e.g.,
+  # downloadData("LCC2005", modulePath(sim)).
+  # Nothing should be created here that does not create an named object in inputObjects.
+  # Any other initiation procedures should be put in "init" eventType of the doEvent function.
+  # Note: the module developer can use 'sim$.userSuppliedObjNames' in their function below to
+  # selectively skip unnecessary steps because the user has provided those inputObjects in the
+  # simInit call. e.g.,
+  # if (!('defaultColor' %in% sim$.userSuppliedObjNames)) {
+  #  sim$defaultColor <- 'red'
+  # }
+  # ! ----- EDIT BELOW ----- ! #
+  
+  # Filenames
+  nbdbPointFilename <- "NFDB_point_20171106"
+  
+  # Also extract
+  nfdbPointAlsoExtract <- basename(paste0(tools::file_path_sans_ext(nbdbPointFilename), ".", c("dbf", "prj", "sbn", "sbx", "shx")))
+  
+  sim$NFDB_point <- Cache(prepInputs,
+                          targetFile = nbdbPointFilename,
+                          archive = "NFDB_point.zip", 
+                          alsoExtract = nfdbPointAlsoExtract,
+                          dataset = "NFDB_PT",
+                          modulePath = modulePath(sim),
+                          moduleName = currentModule(sim),
+                          studyArea = sim$studyArea,
+                          writeCropped = TRUE, 
+                          cacheTags = c("stable", currentModule(sim)),
+                          quick = .quickCheck)
+  
+  
+  # ! ----- STOP EDITING ----- ! #
+  return(invisible(sim))
+}
+### add additional events as needed by copy/pasting from above
