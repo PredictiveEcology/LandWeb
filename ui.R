c65d986fcf08ab263a49594dc207201975f20fe4
Alex Chubaty <alex.chubaty@gmail.com>
2018-02-14 14:03:11 -0700

    add title to app

--- ui.R
+++ ui.R
@@ -4,35 +4,26 @@ library(shinyBS)
 
 dashboardPage(
   skin = "green",
-  dashboardHeader(title = "templateApp"),
+  dashboardHeader(title = "LandWeb"),
   dashboardSidebar(
     width = 300,
-    sidebarMenu(
-      id = "wholeThing",
-      menuItem("Maps - time since fire", tabName = "timeSinceFire", icon = icon("map-o")),
-      menuItem("Large Patches", tabName = "largePatches", icon = icon("bar-chart")),
-      menuItem("Overview Diagrams", tabName = "simInfo", icon = icon("sitemap")),
-      menuItem("Module Info", tabName = "moduleInfo", icon = icon("puzzle-piece")),
-      menuItem("LBMR (Succession) Model Inputs", tabName = "inputTables", icon = icon("table"))
+    sidebarMenu(id = "wholeThing",
+                menuItem("Maps - time since fire", tabName = "timeSinceFire", icon = icon("map-o")),
+menuItem("Large Patches", tabName = "largePatches", icon = icon("bar-chart")),
+menuItem("Overview Diagrams", tabName = "simInfo", icon = icon("sitemap")),
+menuItem("Module Info", tabName = "moduleInfo", icon = icon("puzzle-piece")),
+menuItem("LBMR (Succession) Model Inputs", tabName = "inputTables", icon = icon("table"))
     ),
-    HTML(paste(
-      "<footer>",
-      "<div id=\"sidebar\" style='position: absolute; bottom: 5px; margin: 15px'>",
-      " Built with ", # nolint
-      "<a href=\"http://SpaDES.PredictiveEcology.org\">  <img src=\"http://predictiveecology.org/img/avatar.png\", height=25px>SpaDES, </a>",
-      "<a href=\"http://shiny.rstudio.com/\", target=\"_blank\">shiny</a> and ",
-      "<a href=\"https://appsilondatascience.com/\">  <img src=\"http://d3u4jj2f3q2139.cloudfront.net/logo-appsilon-data-science-transparent.png\", height=20px> </a>",
-      "</div>",
-      "</footer>"
-    ))
+    sidebarFooter()
   ),
   dashboardBody(
     tabItems(
       tabItem("timeSinceFire", timeSinceFireUI("timeSinceFire", length(tsf))),
-      tabItem("largePatches", largePatchesUI("largePatches")),
-      tabItem("simInfo", simInfoUI("simInfo")),
-      tabItem("moduleInfo", moduleInfoUI("moduleInfo")),
-      tabItem("inputTables", inputTablesUI("inputTables"))
+tabItem("largePatches", largePatchesUI("largePatches")),
+tabItem("simInfo", simInfoUI("simInfo")),
+tabItem("moduleInfo", moduleInfoUI("moduleInfo")),
+tabItem("inputTables", inputTablesUI("inputTables"))
     )
   )
 )
+
