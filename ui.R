11c43da72e1da5adba6b3d8f283f2d933d7de717
Alex Chubaty <alex.chubaty@gmail.com>
2018-02-15 12:48:39 -0700

    rename 'timeSinceFire' to 'timeSeriosofRasters'
    with https://github.com/PredictiveEcology/SpaDES.shiny/issues/79

--- ui.R
+++ ui.R
@@ -7,22 +7,23 @@ dashboardPage(
   dashboardHeader(title = "LandWeb"),
   dashboardSidebar(
     width = 300,
-    sidebarMenu(id = "wholeThing",
-                menuItem("Maps - time since fire", tabName = "timeSinceFire", icon = icon("map-o")),
-menuItem("Large Patches", tabName = "largePatches", icon = icon("bar-chart")),
-menuItem("Overview Diagrams", tabName = "simInfo", icon = icon("sitemap")),
-menuItem("Module Info", tabName = "moduleInfo", icon = icon("puzzle-piece")),
-menuItem("LBMR (Succession) Model Inputs", tabName = "inputTables", icon = icon("table"))
+    sidebarMenu(
+      id = "wholeThing",
+      menuItem("Maps - time since fire", tabName = "timeSinceFire", icon = icon("map-o")),
+      menuItem("Large Patches", tabName = "largePatches", icon = icon("bar-chart")),
+      menuItem("Overview Diagrams", tabName = "simInfo", icon = icon("sitemap")),
+      menuItem("Module Info", tabName = "moduleInfo", icon = icon("puzzle-piece")),
+      menuItem("LBMR (Succession) Model Inputs", tabName = "inputTables", icon = icon("table"))
     ),
     sidebarFooter()
   ),
   dashboardBody(
     tabItems(
-      tabItem("timeSinceFire", timeSinceFireUI("timeSinceFire", length(tsf))),
-tabItem("largePatches", largePatchesUI("largePatches")),
-tabItem("simInfo", simInfoUI("simInfo")),
-tabItem("moduleInfo", moduleInfoUI("moduleInfo")),
-tabItem("inputTables", inputTablesUI("inputTables"))
+      #tabItem("timeSinceFire", timeSeriesofRastersUI("timeSinceFire", length(tsf))),
+      tabItem("largePatches", largePatchesUI("largePatches")),
+      tabItem("simInfo", simInfoUI("simInfo")),
+      tabItem("moduleInfo", moduleInfoUI("moduleInfo")),
+      tabItem("inputTables", inputTablesUI("inputTables"))
     ),
 	  copyrightFooterUI("copyright")
   )
