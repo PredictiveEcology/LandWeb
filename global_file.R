7092654b9260bc68d7ea181b4dc6c2bb41759722
Eliot McIntire <eliotmcintire@gmail.com>
2018-02-16 01:11:27 -0800

    makeLeafletTiles

--- global_file.R
+++ global_file.R
@@ -1,6 +1,6 @@
 # List modules first, so we can get all their dependencies
 modules <- list("landWebDataPrep", "initBaseMaps", "fireDataPrep", "LandMine",
-                "Boreal_LBMRDataPrep", "LBMR", "timeSinceFire", "LandWebOutput")
+                "Boreal_LBMRDataPrep", "LBMR", "timeSinceFire", "LandWebOutput")#, "makeLeafletTiles")
 # Spatial stuff -- determines the size of the area that will be "run" in the simulations
 studyArea <- "RIA"  #other options: "FULL", "EXTRALARGE", "LARGE", "MEDIUM", "NWT", "SMALL" , "RIA"
 studyArea <- "VERYSMALL"  #other options: "FULL", "EXTRALARGE", "LARGE", "MEDIUM", "NWT", "SMALL" , "RIA", "VERYSMALL"
