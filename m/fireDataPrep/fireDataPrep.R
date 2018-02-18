35bfcddf76b2767d079b7e1c0cd139deb58e355e
Eliot McIntire <eliotmcintire@gmail.com>
2018-02-18 00:57:51 -0800

    fireDataPrep -- filling in Metadata

--- m/fireDataPrep/fireDataPrep.R
+++ m/fireDataPrep/fireDataPrep.R
@@ -1,41 +1,89 @@
 
+
+
+
+
 # Everything in this file gets sourced during simInit, and all functions and objects
 # are put into the simList. To use objects and functions, use sim$xxx.
-defineModule(sim, list(
-  name = "fireDataPrep",
-  description = "basic data preparation for LCC05 based fire models",
-  keywords = c("LCC05"),
-  authors = c(person(c("Steve", "G"), "Cumming", email = "stevec@sbf.ulaval.ca", role = c("aut", "cre"))),
-  childModules = character(),
-  version = numeric_version("1.2.0.9005"),
-  spatialExtent = raster::extent(rep(NA_real_, 4)),
-  timeframe = as.POSIXlt(c(NA, NA)),
-  timeunit = "year",
-  citation = list("citation.bib"),
-  documentation = list("README.txt", "fireDataPrep.Rmd"),
-  reqdPkgs = list("raster","sp", "SpaDES.tools"),
-  parameters = rbind(
-    #defineParameter("paramName", "paramClass", value, min, max, "parameter description")),
-    defineParameter(".plotInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first plot event should occur"),
-    defineParameter(".plotInterval", "numeric", NA, NA, NA, "This describes the simulation time at which the first plot event should occur"),
-    defineParameter(".saveInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first save event should occur"),
-    defineParameter(".saveInterval", "numeric", NA, NA, NA, "This describes the simulation time at which the first save event should occur"),
-    defineParameter(".useCache", "logical", FALSE, NA, NA, "Whether the module should be cached for future calls. This is generally intended for data-type modules, where stochasticity and time are not relevant")
-  ),
-  inputObjects = data.frame(
-    objectName = c("LCC05", "shpStudyRegion", "rstStudyRegion"),
-    objectClass = c("RasterLayer","SpatialPolygonsDataFrame", "RasterLayer"),
-    sourceURL = "",
-    other = NA_character_,
-    stringsAsFactors = FALSE
-  ),
-  outputObjects = data.frame(
-    objectName = c("rstFlammable"),#, "rstBurnProb"),
-    objectClass = c("RasterLayer"),
-    other = c("rstFlammable is 0 for flammable, 1 for non flammable",""),
-    stringsAsFactors = FALSE
+defineModule(
+  sim,
+  list(
+    name = "fireDataPrep",
+    description = "basic data preparation for LCC05 based fire models",
+    keywords = c("LCC05"),
+    authors = c(person(
+      c("Steve", "G"),
+      "Cumming",
+      email = "stevec@sbf.ulaval.ca",
+      role = c("aut", "cre")
+    )),
+    childModules = character(),
+    version = numeric_version("1.2.0.9005"),
+    spatialExtent = raster::extent(rep(NA_real_, 4)),
+    timeframe = as.POSIXlt(c(NA, NA)),
+    timeunit = "year",
+    citation = list("citation.bib"),
+    documentation = list("README.txt", "fireDataPrep.Rmd"),
+    reqdPkgs = list("raster", "sp", "SpaDES.tools"),
+    parameters = rbind(
+      #defineParameter("paramName", "paramClass", value, min, max, "parameter description")),
+      defineParameter(
+        ".plotInitialTime",
+        "numeric",
+        NA,
+        NA,
+        NA,
+        "This describes the simulation time at which the first plot event should occur"
+      ),
+      defineParameter(
+        ".plotInterval",
+        "numeric",
+        NA,
+        NA,
+        NA,
+        "This describes the simulation time at which the first plot event should occur"
+      ),
+      defineParameter(
+        ".saveInitialTime",
+        "numeric",
+        NA,
+        NA,
+        NA,
+        "This describes the simulation time at which the first save event should occur"
+      ),
+      defineParameter(
+        ".saveInterval",
+        "numeric",
+        NA,
+        NA,
+        NA,
+        "This describes the simulation time at which the first save event should occur"
+      ),
+      defineParameter(
+        ".useCache",
+        "logical",
+        FALSE,
+        NA,
+        NA,
+        "Whether the module should be cached for future calls. This is generally intended for data-type modules, where stochasticity and time are not relevant"
+      )
+    ),
+    inputObjects = data.frame(
+      objectName = c("LCC05", "shpStudyRegion", "rstStudyRegion"),
+      objectClass = c("RasterLayer", "SpatialPolygonsDataFrame", "RasterLayer"),
+      sourceURL = "",
+      other = NA_character_,
+      stringsAsFactors = FALSE
+    ),
+    outputObjects = data.frame(
+      objectName = c("rstFlammable"),
+      #, "rstBurnProb"),
+      objectClass = c("RasterLayer"),
+      other = c("rstFlammable is 0 for flammable, 1 for non flammable", ""),
+      stringsAsFactors = FALSE
+    )
   )
-))
+)
 
 ## event types
 #   - type `init` is required for initialiazation
@@ -47,8 +95,14 @@ doEvent.fireDataPrep = function(sim, eventTime, eventType, debug = FALSE) {
     # do stuff for this event
     sim <- Init(sim)
   } else {
-    warning(paste("Undefined event type: '", current(sim)[1, "eventType", with = FALSE],
-                  "' in module '", current(sim)[1, "moduleName", with = FALSE], "'", sep = ""))
+    warning(paste(
+      "Undefined event type: '",
+      current(sim)[1, "eventType", with = FALSE],
+      "' in module '",
+      current(sim)[1, "moduleName", with = FALSE],
+      "'",
+      sep = ""
+    ))
   }
   return(invisible(sim))
 }
@@ -61,27 +115,34 @@ doEvent.fireDataPrep = function(sim, eventTime, eventType, debug = FALSE) {
 ### template initialization
 
 Init <- function(sim) {
-  nonFlammClasses<-c(36,37,38,39)
+  nonFlammClasses <- c(36, 37, 38, 39)
   oldClass <- 0:39
-  newClass <- ifelse(oldClass %in% nonFlammClasses,1,0)   #1 codes for non flammable
+  newClass <-
+    ifelse(oldClass %in% nonFlammClasses, 1, 0)   #1 codes for non flammable
   #see mask argument for SpaDES::spread()
   flammableTable <- cbind(oldClass, newClass)
   #according to Yong, Canada Landcover 2005 is loaded as LCC05
-  sim$rstFlammable <- ratify(reclassify(sim$LCC05, flammableTable,count=TRUE))
-  sim$rstFlammable <- writeRaster(sim$rstFlammable, filename = file.path(outputPath(sim), "rstFlammable"),
-                                  overwrite = TRUE)
-
-  setColors(sim$rstFlammable,n=2) <- colorRampPalette(c("blue", "red"))(2)
+  sim$rstFlammable <-
+    ratify(reclassify(sim$LCC05, flammableTable, count = TRUE))
+  sim$rstFlammable <-
+    writeRaster(
+      sim$rstFlammable,
+      filename = file.path(outputPath(sim), "rstFlammable"),
+      overwrite = TRUE
+    )
+  
+  setColors(sim$rstFlammable, n = 2) <-
+    colorRampPalette(c("blue", "red"))(2)
   sim$rstFlammable[is.na(sim$rstStudyRegion[])] <- NA
-
+  
   # Much faster than call rasterize again
   return(invisible(sim))
 }
 
 
 .inputObjects = function(sim) {
-
   # if (is.null(sim$shpStudyRegion)) {
+  
   #   sim$shpStudyRegion <- randomPolygon(matrix(c(-90, 60), ncol = 2), 5)
   # }
   # if (is.null(sim$LCC05)) {
@@ -90,8 +151,7 @@ Init <- function(sim) {
   # if (is.null(sim$rstStudyRegion)) {
   #   sim$rstStudyRegion <- rasterize(sim$shpStudyRegion, sim$LCC05)
   # }
-
+  
   return(invisible(sim))
 }
 ### add additional events as needed by copy/pasting from above
-
