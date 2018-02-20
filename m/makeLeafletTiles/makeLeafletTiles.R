0e0ef40f358b575b9b5293f25c13883f99c5fc91
Alex Chubaty <alex.chubaty@gmail.com>
2018-02-20 14:08:51 -0700

    need 'makeTiles` function from makeLeafletTiles module

--- m/makeLeafletTiles/makeLeafletTiles.R
+++ m/makeLeafletTiles/makeLeafletTiles.R
@@ -52,27 +52,3 @@ doEvent.makeLeafletTiles = function(sim, eventTime, eventType) {
   return(invisible(sim))
 }
 
-makeTiles <- function(sim) {
-  outs <- outputs(sim)
-  savedObjs <- outs$objectName
-  savedObjsUnique <- unique(savedObjs)
-  names(savedObjs) <- savedObjs
-  names(savedObjsUnique) <- savedObjsUnique
-  rastFiles <- outs[grepl("\\.tif|\\.grd", outs$file), "file"]
-  
-  rasts <- lapply(rastFiles, function(r) {
-    raster(r)
-  })
-  outputPath <- file.path("www", "tiles")
-  sim$allRasters <- Cache(lapply, rasts, function(r)
-    gdal2Tiles(
-      r,
-      outputPath,
-      zoomRange = 1:11,
-      colorTableFile = asPath(colorTableFile),
-      rasterForTransparency = asPath(file.path(outputPath(sim), "rstFlammable.grd")),
-      cacheRepo = cachePath(sim)
-    ))
-  return(invisible(sim))
-}
-
