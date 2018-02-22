a947cddb1a7381f274510d23300b448b8b00546f
Eliot McIntire <eliotmcintire@gmail.com>
2018-02-22 14:05:29 -0800

    accidentally deleted SpaDES.shiny install

--- generateApp.R
+++ generateApp.R
@@ -5,6 +5,7 @@ if (FALSE) { # these are FALSE for standard use, but individual cases may need t
   devtools::install_github("PredictiveEcology/SpaDES.tools", ref = "prepInputs") 
   devtools::install_github("PredictiveEcology/SpaDES.core", ref = "development") 
 } 
+devtools::install_github("PredictiveEcology/SpaDES.shiny", ref = "develop")
 
 if (FALSE) {
   ## test download of private data from Google Drive
