e243de7f73869cd46bc74626abdb9f799288a5e9
Eliot McIntire <eliotmcintire@gmail.com>
2018-03-16 16:42:05 -0700

    better gdal_setInstallation("/usr/bin")

--- generateApp.R
+++ generateApp.R
@@ -18,7 +18,24 @@ if (FALSE) {
 }
 
 library(dplyr)
-library(gdalUtils); gdal_setInstallation(rescan = TRUE)
+library(gdalUtils)
+commonLinuxGdalPath <- "/usr/bin"
+commonWindowsGdalPath <- "C:/OSGEO4~1/bin/"
+if (dir.exists(commonLinuxGdalPath)) {
+  gdal_setInstallation(commonLinuxGdalPath)
+} else {
+  if (dir.exists(commonWindowsGdalPath)) {
+    gdal_setInstallation(commonWindowsGdalPath)
+    if (is.null(getOption("gdalUtils_gdalPath"))) {
+      gdal_setInstallation(rescan = TRUE)  
+    } 
+  }
+}
+if (is.null(getOption("gdalUtils_gdalPath"))) {
+  gdal_setInstallation(rescan = TRUE)  
+}
+
+  
 library(SpaDES.shiny)
 
 #load_all("~/GitHub/SpaDES.shiny")
