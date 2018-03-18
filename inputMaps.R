57e79f44b7b45286fef68b0bc870c45c055b239c
Eliot McIntire <eliotmcintire@gmail.com>
2018-03-17 20:23:31 -0700

    LandMine Fire Return Inteval <= 30 years not good

--- inputMaps.R
+++ inputMaps.R
@@ -41,7 +41,8 @@ loadStudyRegion <- function(shpPath, fireReturnIntervalMap, studyArea, crsKNNMap
   if (!isTRUE("LTHRC" %in% names(shpStudyRegionFull))) {
     shpStudyRegionFull$LTHRC <- shpStudyRegionFull$LTHFC # Apparently, sometimes it is LTHFC, sometimes LTHRC
     shpStudyRegionFull$LTHFC <- NULL
-    shpStudyRegionFull$LTHRC[shpStudyRegionFull$LTHRC <= 20] <- NA
+    # The fires of Fire Return Interval 30 years are not correctly simulated by LandMine, so they are removed.
+    shpStudyRegionFull$LTHRC[shpStudyRegionFull$LTHRC <= 30] <- NA
   }
   shpStudyRegionFull$fireReturnInterval <- shpStudyRegionFull$LTHFC
   shpStudyRegionFull@data <- shpStudyRegionFull@data[, !(names(shpStudyRegionFull) %in% "ECODISTRIC")]
