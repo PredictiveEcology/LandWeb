b894c054feae3ccf37b861b847faf6e4db9a4da5
Eliot McIntire <eliotmcintire@gmail.com>
2018-03-22 18:24:48 -0700

    Thin polygons -- still experimental

--- mapsForShiny.R
+++ mapsForShiny.R
@@ -4,14 +4,14 @@ lflt <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 # Alberta Ecozone
 dPath <- asPath(file.path(paths$inputPath, "ecozones", "Alberta"))
 albertaEcozoneFiles <- asPath(c("Natural_Regions_Subregions_of_Alberta.dbf", 
-                           "Natural_Regions_Subregions_of_Alberta.lyr", "Natural_Regions_Subregions_of_Alberta.prj", 
-                           "Natural_Regions_Subregions_of_Alberta.shp.xml", 
-                           "Natural_Regions_Subregions_of_Alberta.shx", "natural_regions_subregions_of_alberta.zip", 
-                           "nsr2005_final_letter.jpg", "nsr2005_final_letter.pdf"))
+                                "Natural_Regions_Subregions_of_Alberta.lyr", "Natural_Regions_Subregions_of_Alberta.prj", 
+                                "Natural_Regions_Subregions_of_Alberta.shp.xml", 
+                                "Natural_Regions_Subregions_of_Alberta.shx", "natural_regions_subregions_of_alberta.zip", 
+                                "nsr2005_final_letter.jpg", "nsr2005_final_letter.pdf"))
 albertaEcozoneURL <- "https://www.albertaparks.ca/media/429607/natural_regions_subregions_of_alberta.zip"
 albertaEcozoneFilename <- asPath("Natural_Regions_Subregions_of_Alberta.shp")
 shpAlbertaEcozone <- Cache(prepInputs, url = albertaEcozoneURL, targetFile = albertaEcozoneFilename,
-           fun = "shapefile", destinationPath = dPath, alsoExtract = albertaEcozoneFiles)
+                           fun = "shapefile", destinationPath = dPath, alsoExtract = albertaEcozoneFiles)
 shpAlbertaEcozone@data[[labelColumn]] <- shpAlbertaEcozone$NSRNAME
 
 # Nationa Ecozone
@@ -20,20 +20,20 @@ ecozoneFilename <-   file.path(dPath, "ecozones.shp")
 ecozoneFiles <- c("ecozones.dbf", "ecozones.prj", 
                   "ecozones.sbn", "ecozones.sbx", "ecozones.shp", "ecozones.shx")
 shpNationalEcozone <- Cache(prepInputs, url = "http://sis.agr.gc.ca/cansis/nsdb/ecostrat/zone/ecozone_shp.zip",
-      targetFile = asPath(ecozoneFilename),
-      alsoExtract = ecozoneFiles,
-      fun = "shapefile", destinationPath = dPath)
+                            targetFile = asPath(ecozoneFilename),
+                            alsoExtract = ecozoneFiles,
+                            fun = "shapefile", destinationPath = dPath)
 shpNationalEcozone@data[[labelColumn]] <- shpNationalEcozone$ZONE_NAME
 
 # Nationa Ecodistrict
 dPath <- file.path(paths$inputPath, "ecodistricts", "National")
 ecodistrictFilename <-   file.path(dPath, "ecodistricts.shp")
 ecodistrictFiles <- c("ecodistricts.dbf", "ecodistricts.prj", 
-                  "ecodistricts.sbn", "ecodistricts.sbx", "ecodistricts.shp", "ecodistricts.shx")
+                      "ecodistricts.sbn", "ecodistricts.sbx", "ecodistricts.shp", "ecodistricts.shx")
 shpNationalEcodistrict <- Cache(prepInputs, url = "http://sis.agr.gc.ca/cansis/nsdb/ecostrat/district/ecodistrict_shp.zip",
-                            targetFile = asPath(ecodistrictFilename),
-                            alsoExtract = ecodistrictFiles,
-                            fun = "shapefile", destinationPath = dPath)
+                                targetFile = asPath(ecodistrictFilename),
+                                alsoExtract = ecodistrictFiles,
+                                fun = "shapefile", destinationPath = dPath)
 shpNationalEcodistrict@data[[labelColumn]] <- shpNationalEcodistrict$ZONE_NAME
 
 
@@ -62,11 +62,11 @@ albertaFMUFiles <- c("FMU_Alberta_2015-11.cpg", "FMU_Alberta_2015-11.dbf",
                      "FMU_Alberta_2015-11.sbx", "FMU_Alberta_2015-11.shp", 
                      "FMU_Alberta_2015-11.shp.xml", "FMU_Alberta_2015-11.shx")
 shpAlbertaFMU <- Cache(prepInputs, 
-                         url = "https://drive.google.com/file/d/1JiCLcHh5fsBAy8yAx8NgtK7fxaZ4Tetl/view?usp=sharing",
-                         targetFile = albertaFMUFilename,
-                         alsoExtract = albertaFMUFiles,
-                         fun = "shapefile", 
-                         destinationPath = dPath)
+                       url = "https://drive.google.com/file/d/1JiCLcHh5fsBAy8yAx8NgtK7fxaZ4Tetl/view?usp=sharing",
+                       targetFile = albertaFMUFilename,
+                       alsoExtract = albertaFMUFiles,
+                       fun = "shapefile", 
+                       destinationPath = dPath)
 shpAlbertaFMU@data[[labelColumn]] <- shpAlbertaFMU$FMU_NAME
 
 # Put all polygons together in a list
@@ -81,8 +81,8 @@ polygons <- Cache(lapply, polygons, function(shp) {
 })
 
 # Make SubRegion
-  polygonsSubRegion <- Cache(intersectListShps, polygons, shpStudyRegion)
-  names(polygonsSubRegion) <- paste0(names(polygonsSubRegion), "Demo")
+polygonsSubRegion <- Cache(intersectListShps, polygons, shpStudyRegion)
+names(polygonsSubRegion) <- paste0(names(polygonsSubRegion), "Demo")
 polygons <- append(polygons, polygonsSubRegion)
 
 
@@ -94,7 +94,6 @@ names(polygonsLflt) <- paste0(names(polygonsLflt), "LFLT")
 polygons <- append(polygons, polygonsLflt)
 
 
-browser()
 availableScales <- c("Full", "Demo")
 
 available <- data.frame(stringsAsFactors = FALSE,
@@ -103,11 +102,13 @@ available <- data.frame(stringsAsFactors = FALSE,
                                     scales = availableScales,
                                     projections = availableProjections),
                         names = names(polygons)
-                        )
+)
 
 #### Thin polygons
-if (FALSE)
-  a <- thin(shpNationalEcozone)
+if (FALSE) {
+  browser()
+  polygonsThin <- lapply(polygons, thin, 20)
+}
 
 
 
@@ -138,80 +139,80 @@ if (FALSE) {
 #availablePolygons <- c("ecodistricts")#, "AlbertaFMU")
 if (FALSE) { #NOT USED
   availablePolygonAdjective <- c("Ecodistrict")#, "AlbertaFMU")
-}
-
-rm(ecodistrictsFull)
-rm(ecodistrictsFullLFLT)
-
-polygonColours <- c(rep(c("red", "blue"), 2))
-polygonIndivIdsColum <- list("ECODISTRIC", "FMU_NAME") %>%
-  setNames(names(polygons[1:(length(polygons) / 4) + (length(polygons) / 4) * 3]))
-
-colorVec <- diff(c(ageClassCutOffs[1],10,30,50,ageClassCutOffs[-(1:4)],maxAge))
-
-timeSinceFirePalette <- leaflet::colorNumeric(
-  na.color = "transparent",
-  c(rep("red", colorVec[1]), rep("orange", colorVec[2]), rep("yellow", colorVec[3]),
-    paste0(colorRampPalette(c("light green", "dark green"))(colorVec[4]), "FF")),
-  domain = NULL
-)
-
-fireReturnIntervalPalette <- leaflet::colorFactor(
-  "Spectral", shpStudyRegionFull$fireReturnInterval
-)
-
-colorTableFile <<- file.path("www", studyArea, "color_table.txt")
-checkPath(dirname(colorTableFile), create = TRUE)
-color_tableFn <- function(timeSinceFirePalette, maxAge) {
-  a <- t(sapply(timeSinceFirePalette(1:maxAge), col2rgb))
-  rownames(a) <- NULL
-  a <- rbind(rep(0,4), cbind(1:maxAge, a))
-  write.table(a, file = colorTableFile, append = FALSE, row.names = FALSE, col.names = FALSE)
-}
-color_tableFn(timeSinceFirePalette, maxAge)
-
-
-
-if (FALSE) {
-  readSpTransform <- function(shapefilePath, crs, cacheRepo){
-    AlbertaFMUFull <- shapefile(shapefilePath)
-    AlbertaFMUFull <- spTransform(AlbertaFMUFull, crs)
+  
+  rm(ecodistrictsFull)
+  rm(ecodistrictsFullLFLT)
+  
+  polygonColours <- c(rep(c("red", "blue"), 2))
+  polygonIndivIdsColum <- list("ECODISTRIC", "FMU_NAME") %>%
+    setNames(names(polygons[1:(length(polygons) / 4) + (length(polygons) / 4) * 3]))
+  
+  colorVec <- diff(c(ageClassCutOffs[1],10,30,50,ageClassCutOffs[-(1:4)],maxAge))
+  
+  timeSinceFirePalette <- leaflet::colorNumeric(
+    na.color = "transparent",
+    c(rep("red", colorVec[1]), rep("orange", colorVec[2]), rep("yellow", colorVec[3]),
+      paste0(colorRampPalette(c("light green", "dark green"))(colorVec[4]), "FF")),
+    domain = NULL
+  )
+  
+  fireReturnIntervalPalette <- leaflet::colorFactor(
+    "Spectral", shpStudyRegionFull$fireReturnInterval
+  )
+  
+  colorTableFile <<- file.path("www", studyArea, "color_table.txt")
+  checkPath(dirname(colorTableFile), create = TRUE)
+  color_tableFn <- function(timeSinceFirePalette, maxAge) {
+    a <- t(sapply(timeSinceFirePalette(1:maxAge), col2rgb))
+    rownames(a) <- NULL
+    a <- rbind(rep(0,4), cbind(1:maxAge, a))
+    write.table(a, file = colorTableFile, append = FALSE, row.names = FALSE, col.names = FALSE)
   }
-  AlbertaFMUFull <- Cache(cacheRepo = paths$cachePath,
-                          readSpTransform,
-                          shapefilePath=file.path(paths$inputPath, "FMU_Alberta_2015-11", "FMU_Alberta_2015-11"),
-                          crs = crs(shpStudyRegion))
-  AlbertaFMU <- Cache(crop, AlbertaFMUFull, shpStudyRegion, cacheRepo = paths$cachePath)
+  color_tableFn(timeSinceFirePalette, maxAge)
+  
+  
+  
+  if (FALSE) {
+    readSpTransform <- function(shapefilePath, crs, cacheRepo){
+      AlbertaFMUFull <- shapefile(shapefilePath)
+      AlbertaFMUFull <- spTransform(AlbertaFMUFull, crs)
+    }
+    AlbertaFMUFull <- Cache(cacheRepo = paths$cachePath,
+                            readSpTransform,
+                            shapefilePath=file.path(paths$inputPath, "FMU_Alberta_2015-11", "FMU_Alberta_2015-11"),
+                            crs = crs(shpStudyRegion))
+    AlbertaFMU <- Cache(crop, AlbertaFMUFull, shpStudyRegion, cacheRepo = paths$cachePath)
+    
+  }
+  #lflt <- "+init=epsg:4326"
+  
+  # getEcoMaps <- function(ecoDistrictPath, cacheRepo, lfltEPSG) {
+  #   ecodistricts <- shapefile(ecoDistrictPath)
+  #   ecodistrictsFull <- shapefile(ecoDistrictPath)
+  #   shpStudyRegionEco <- spTransform(shpStudyRegion, crs(ecodistricts))
+  #   
+  #   # There is a self intersection problem with ecodistricts file. This fixes it.
+  #   ecodistricts <- raster::buffer(ecodistricts, width = 0, dissolve = FALSE)
+  #   
+  #   ecodistrictsStudyRegion <- crop(ecodistricts, shpStudyRegionEco)
+  #   #ecodistrictsCan <- spTransform(ecodistrictsStudyRegion, crs(CanadaMap))
+  #   ecodistricts <- spTransform(ecodistrictsStudyRegion, crs(shpStudyRegion))
+  #   
+  #   # Available polygons
+  #   ecodistrictsDemoLFLT <- spTransform(ecodistricts, sp::CRS(lfltEPSG))
+  #   ecodistrictsFullLFLT <- spTransform(ecodistrictsFull, sp::CRS(lfltEPSG))
+  #   #AlbertaFMUDemoLFLT <- spTransform(AlbertaFMU, sp::CRS(lfltEPSG))
+  #   #AlbertaFMUFullLFLT <- spTransform(AlbertaFMUFull, sp::CRS(lfltEPSG))
+  #   ecodistrictsDemo <- ecodistricts
+  #   #AlbertaFMUDemo <- AlbertaFMU
+  #   #AlbertaFMUFull <- AlbertaFMUFull
+  #   list(ecodistricts=ecodistricts,
+  #        ecodistrictsDemo=ecodistrictsDemo,
+  #        ecodistrictsFull=ecodistrictsFull,
+  #        ecodistrictsDemoLFLT=ecodistrictsDemoLFLT,
+  #        ecodistrictsFullLFLT=ecodistrictsFullLFLT
+  #   )
+  #   
+  # }
   
 }
-#lflt <- "+init=epsg:4326"
-
-# getEcoMaps <- function(ecoDistrictPath, cacheRepo, lfltEPSG) {
-#   ecodistricts <- shapefile(ecoDistrictPath)
-#   ecodistrictsFull <- shapefile(ecoDistrictPath)
-#   shpStudyRegionEco <- spTransform(shpStudyRegion, crs(ecodistricts))
-#   
-#   # There is a self intersection problem with ecodistricts file. This fixes it.
-#   ecodistricts <- raster::buffer(ecodistricts, width = 0, dissolve = FALSE)
-#   
-#   ecodistrictsStudyRegion <- crop(ecodistricts, shpStudyRegionEco)
-#   #ecodistrictsCan <- spTransform(ecodistrictsStudyRegion, crs(CanadaMap))
-#   ecodistricts <- spTransform(ecodistrictsStudyRegion, crs(shpStudyRegion))
-#   
-#   # Available polygons
-#   ecodistrictsDemoLFLT <- spTransform(ecodistricts, sp::CRS(lfltEPSG))
-#   ecodistrictsFullLFLT <- spTransform(ecodistrictsFull, sp::CRS(lfltEPSG))
-#   #AlbertaFMUDemoLFLT <- spTransform(AlbertaFMU, sp::CRS(lfltEPSG))
-#   #AlbertaFMUFullLFLT <- spTransform(AlbertaFMUFull, sp::CRS(lfltEPSG))
-#   ecodistrictsDemo <- ecodistricts
-#   #AlbertaFMUDemo <- AlbertaFMU
-#   #AlbertaFMUFull <- AlbertaFMUFull
-#   list(ecodistricts=ecodistricts,
-#        ecodistrictsDemo=ecodistrictsDemo,
-#        ecodistrictsFull=ecodistrictsFull,
-#        ecodistrictsDemoLFLT=ecodistrictsDemoLFLT,
-#        ecodistrictsFullLFLT=ecodistrictsFullLFLT
-#   )
-#   
-# }
-
