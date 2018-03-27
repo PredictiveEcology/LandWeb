0632b9539e34059f292db9b3aa9e8a8168f91018
Eliot McIntire <eliotmcintire@gmail.com>
2018-03-27 14:51:34 -0700

    clear out mapsForShiny

--- mapsForShiny.R
+++ mapsForShiny.R
@@ -1,35 +1,11 @@
-labelColumn <- "shinyLabel"
-lflt <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
-
-reportingPolygons <- Cache(createReportingPolygons,
-                            c("Alberta Ecozones", "National Ecozones", 
-                            "National Ecodistricts", "Forest Management Areas", 
-                            "Alberta FMUs", "Caribou Herds"))
-  
-########################################################
-########################################################
-### CURRENT CONDITION ##################################
-message("Loading Current Condition Rasters")
-dPath <- file.path(paths$inputPath, "CurrentCondition")
-CCspeciesNames <- c("Pine", "Age", "BlackSpruce", "Deciduous", "Fir", "LandType", "WhiteSpruce")
-rstCurrentConditionList <- Cache(loadCCSpecies, CCspeciesNames, 
-                                 url = "https://drive.google.com/open?id=1JnKeXrw0U9LmrZpixCDooIm62qiv4_G1",
-                                 dPath = dPath)
 
 #############################################################################
 #############################################################################
 
-
-#availablePolygons <- c("ecodistricts")#, "AlbertaFMU")
-if (FALSE) { #NOT USED
-  availablePolygonAdjective <- c("Ecodistrict")#, "AlbertaFMU")
-  
-  rm(ecodistrictsFull)
-  rm(ecodistrictsFullLFLT)
-  
-  polygonColours <- c(rep(c("red", "blue"), 2))
-  polygonIndivIdsColum <- list("ECODISTRIC", "FMU_NAME") %>%
-    setNames(names(polygons[1:(length(polygons) / 4) + (length(polygons) / 4) * 3]))
+if (FALSE) { #Required
+  #polygonColours <- c(rep(c("red", "blue"), 2))
+  #polygonIndivIdsColum <- list("ECODISTRIC", "FMU_NAME") %>%
+  #  setNames(names(polygons[1:(length(polygons) / 4) + (length(polygons) / 4) * 3]))
   
   colorVec <- diff(c(ageClassCutOffs[1],10,30,50,ageClassCutOffs[-(1:4)],maxAge))
   
@@ -40,9 +16,9 @@ if (FALSE) { #NOT USED
     domain = NULL
   )
   
-  fireReturnIntervalPalette <- leaflet::colorFactor(
-    "Spectral", shpStudyRegionFull$fireReturnInterval
-  )
+  # fireReturnIntervalPalette <- leaflet::colorFactor(
+  #   "Spectral", shpStudyRegionFull$fireReturnInterval
+  # )
   
   colorTableFile <<- file.path("www", studyArea, "color_table.txt")
   checkPath(dirname(colorTableFile), create = TRUE)
@@ -54,19 +30,5 @@ if (FALSE) { #NOT USED
   }
   color_tableFn(timeSinceFirePalette, maxAge)
   
-  
-  
-  if (FALSE) {
-    readSpTransform <- function(shapefilePath, crs, cacheRepo){
-      AlbertaFMUFull <- shapefile(shapefilePath)
-      AlbertaFMUFull <- spTransform(AlbertaFMUFull, crs)
-    }
-    AlbertaFMUFull <- Cache(cacheRepo = paths$cachePath,
-                            readSpTransform,
-                            shapefilePath=file.path(paths$inputPath, "FMU_Alberta_2015-11", "FMU_Alberta_2015-11"),
-                            crs = crs(shpStudyRegion))
-    AlbertaFMU <- Cache(crop, AlbertaFMUFull, shpStudyRegion, cacheRepo = paths$cachePath)
-    
-  }
 
 }
