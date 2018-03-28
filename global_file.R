6875932dcf91f2dfab3eb1cfdc4afb99c8e5db41
Eliot McIntire <eliotmcintire@gmail.com>
2018-03-27 17:13:00 -0700

    add reportingPolygons back

--- global_file.R
+++ global_file.R
@@ -160,3 +160,27 @@ labelColumn <- "shinyLabel"
 lflt <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 
 message("  Finished global.R")
+
+source("mapsForShiny.R") 
+labelColumn <- "shinyLabel" 
+lflt <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" 
+
+######################################################## 
+######################################################## 
+# formerly in mapsForShiny.R 
+# Reporting polygons 
+message("Loading Reporting Polygons") 
+reportingPolygons <- Cache(createReportingPolygons, 
+                           c("Alberta Ecozones", "National Ecozones",  
+                             "National Ecodistricts", "Forest Management Areas",  
+                             "Alberta FMUs", "Caribou Herds"),
+                           shpStudyRegion = shpStudyRegionFull, # change the names here
+                           shpSubStudyRegion = shpStudyRegion) 
+
+### CURRENT CONDITION ################################## 
+message("Loading Current Condition Rasters") 
+dPath <- file.path(paths$inputPath, "CurrentCondition") 
+CCspeciesNames <- c("Pine", "Age", "BlackSpruce", "Deciduous", "Fir", "LandType", "WhiteSpruce") 
+rstCurrentConditionList <- Cache(loadCCSpecies, CCspeciesNames,  
+                                 url = "https://drive.google.com/open?id=1JnKeXrw0U9LmrZpixCDooIm62qiv4_G1", 
+                                 dPath = dPath) 
