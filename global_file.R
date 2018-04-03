69625abaf46f9e5e344be0e73f2942a0c9e0d57f
Eliot McIntire <eliotmcintire@gmail.com>
2018-04-02 21:46:10 -0700

    30 years, Free & Proprietary

--- global_file.R
+++ global_file.R
@@ -1,10 +1,10 @@
 # Overall model times # start is default at 0
-endTime <- 2
-summaryInterval <- 1
-summaryPeriod <- c(1, endTime)
+endTime <- 30
+summaryInterval <- 5
+summaryPeriod <- c(20, endTime)
 
 # cacheId for 1000 years: 2e35699c4ade1b4bfa82e864558c7436, 7.3 days - on 342
-authenticationType <- list("Free") # Can do one or both of "Free" "Proprietary"
+authenticationType <- list("Free", "Proprietary") # Can do one or both of "Free" "Proprietary"
 # Spatial stuff -- determines the size of the area that will be "run" in the simulations
 subStudyRegionName <- "SMALL"  #other options: "FULL", "EXTRALARGE", "LARGE", "MEDIUM", "NWT", "SMALL" , "RIA"
                               #other options: "BC", "AB", "SK", "MB" or combinations, please specify in West-East order
