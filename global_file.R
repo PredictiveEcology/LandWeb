ac29494b081f374ebc79d2b2483890034f6fbcae
Eliot McIntire <eliotmcintire@gmail.com>
2018-04-04 09:00:28 -0700

    Optionally take subStudyRegionName from environment var

--- global_file.R
+++ global_file.R
@@ -6,8 +6,11 @@ summaryPeriod <- c(20, endTime)
 # cacheId for 1000 years: 2e35699c4ade1b4bfa82e864558c7436, 7.3 days - on 342
 authenticationType <- list("Free", "Proprietary") # Can do one or both of "Free" "Proprietary"
 # Spatial stuff -- determines the size of the area that will be "run" in the simulations
-subStudyRegionName <- "SMALL"  #other options: "FULL", "EXTRALARGE", "LARGE", "MEDIUM", "NWT", "SMALL" , "RIA"
-                              #other options: "BC", "AB", "SK", "MB" or combinations, please specify in West-East order
+subStudyRegionName <- if (nzchar(Sys.getenv("subStudyRegionName"))) {
+  Sys.getenv("subStudyRegionName")
+} else {
+  "SMALL"  #other options: "FULL", "EXTRALARGE", "LARGE", "MEDIUM", "NWT", "SMALL" , "RIA"
+}                              #other options: "BC", "AB", "SK", "MB" or combinations, please specify in West-East order
 
 
 # Packages for global.R -- don't need to load packages for modules -- happens automatically
