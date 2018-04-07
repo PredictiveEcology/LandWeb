78a6be138229800658e8a001c480ea61af7b7291
Eliot McIntire <eliotmcintire@gmail.com>
2018-04-06 20:12:11 -0700

    more app stuff

--- BC_parameters.R
+++ BC_parameters.R
@@ -1,4 +1,5 @@
 source("ThousandYrFreeProprietary.R")
 subStudyRegionName <- "BC"
-cacheIds4Experiment <- list("da34e6451516dfb638d00539af72d571", NULL)
+authenticationType <- list("Free") # Can do one or both of "Free" "Proprietary"
+cacheIds4Experiment <- list("da34e6451516dfb638d00539af72d571")
 #cacheId4simInitAndExperiment <- "65315fcf61a776691cda53606b6ac3b3"
