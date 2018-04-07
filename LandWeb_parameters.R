22878fc30929cba7457bfa79197d56845353d9a4
Eliot McIntire <eliotmcintire@gmail.com>
2018-04-07 09:57:10 -0700

    updates (sorry for few details)

--- LandWeb_parameters.R
+++ LandWeb_parameters.R
@@ -2,4 +2,5 @@ source("ThousandYrFreeProprietary.R")
 subStudyRegionName <- "FULL"
 authenticationType <- list("Free", "Proprietary") # Can do one or both of "Free" "Proprietary"
 cacheIds4Experiment <- list("53d4b5f8645c379326613b182c6b660d", "2e35699c4ade1b4bfa82e864558c7436")
+cacheIdTsfRasters <- "cc0497d32c2828c81d0eab88d8a7de00"
 #cacheId4simInitAndExperiment <- "65315fcf61a776691cda53606b6ac3b3"
