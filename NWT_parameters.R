ad3a324b0daf45516468837a6668d2f5a3b34ed8
Eliot McIntire <eliotmcintire@gmail.com>
2018-04-07 15:20:07 -0700

    4 cluster nodes for raster::extract

--- NWT_parameters.R
+++ NWT_parameters.R
@@ -1,3 +1,4 @@
+numClusters <- 4
 source("ThousandYrFreeProprietary.R")
 subStudyRegionName <- "NWT"
 cacheIds4Experiment <- list("9e4d7ce672db44205ebff39888ae0d42", "43c1553ee50337305b6fb5d05cc87cc4")
