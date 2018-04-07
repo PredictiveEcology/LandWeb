8734ad7bd7f5df1b96cbcf3b0281f76338150f0c
Eliot McIntire <eliotmcintire@gmail.com>
2018-04-07 10:27:09 -0700

    MB and simInitAndExperiment

--- MB_parameters.R
+++ MB_parameters.R
@@ -2,3 +2,4 @@ source("ThousandYrFreeProprietary.R")
 subStudyRegionName <- "MB"
 authenticationType <- list("Free", "Proprietary") # Can do one or both of "Free" "Proprietary"
 cacheIds4Experiment <- list("9cda3906229ad6760fd7da9ff910eadc", NULL)
+cacheId4Experiment <- "d1d76d98ee6f9642f4fd9c90435b1f60"
