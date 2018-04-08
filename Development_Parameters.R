5528c36f38ade54b0952e4b2e74c7a88d6929619
Eliot McIntire <eliotmcintire@gmail.com>
2018-04-07 19:48:07 -0700

    bugfixes for cl stuff

--- Development_Parameters.R
+++ Development_Parameters.R
@@ -2,6 +2,7 @@
 endTime <- 30
 summaryInterval <- 5
 summaryPeriod <- c(20, endTime)
+useParallelCluster <- FALSE
 
 # cacheId for 1000 years: 2e35699c4ade1b4bfa82e864558c7436, 7.3 days - on 342
 authenticationType <- list("Free", "Proprietary") # Can do one or both of "Free" "Proprietary"
