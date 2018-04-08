c87ae58c05ea9db161eefac50ec0315dee306aca
Eliot McIntire <eliotmcintire@gmail.com>
2018-04-08 10:31:33 -0700

    convert paths to Windows

--- detailsOfRuns.R
+++ detailsOfRuns.R
@@ -348,15 +348,15 @@ rsync -avzh emcintir@132.156.148.172:/home/emcintir/Documents/GitHub/LandWeb/www
 ################################# -- delete
 
 # MB
-rsync -avzh emcintir@132.156.149.44:/home/emcintir/Documents/GitHub/LandWeb/cache/MB/* cache/MB/ --delete
+rsync -avzh emcintir@132.156.148.172:/home/emcintir/Documents/GitHub/LandWeb/cache/MB/* cache/MB/ --delete
 # MB Proprietary
-rsync -avzh emcintir@132.156.149.44:/home/emcintir/Documents/GitHub/LandWeb/cache/MB_Proprietary/* cache/MB_Proprietary/ --delete
-rsync -avzh emcintir@132.156.149.44:/home/emcintir/Documents/GitHub/LandWeb/outputs/MB_Proprietary/* outputs/MB_Proprietary/ --delete
-rsync -avzh emcintir@132.156.149.44:/home/emcintir/Documents/GitHub/LandWeb/www/Proprietary/MB/* www/Proprietary/MB/ --delete
+rsync -avzh emcintir@132.156.148.172:/home/emcintir/Documents/GitHub/LandWeb/cache/MB_Proprietary/* cache/MB_Proprietary/ --delete
+rsync -avzh emcintir@132.156.148.172:/home/emcintir/Documents/GitHub/LandWeb/outputs/MB_Proprietary/* outputs/MB_Proprietary/ --delete
+rsync -avzh emcintir@132.156.148.172:/home/emcintir/Documents/GitHub/LandWeb/www/Proprietary/MB/* www/Proprietary/MB/ --delete
 
 # MB Free
-rsync -avzh emcintir@132.156.149.44:/home/emcintir/Documents/GitHub/LandWeb/cache/MB_Free/* cache/MB_Free/ --delete
-rsync -avzh emcintir@132.156.149.44:/home/emcintir/Documents/GitHub/LandWeb/outputs/MB_Free/* outputs/MB_Free/ --delete
-rsync -avzh emcintir@132.156.149.44:/home/emcintir/Documents/GitHub/LandWeb/www/Free/MB/* www/Free/MB/ --delete
+rsync -avzh emcintir@132.156.148.172:/home/emcintir/Documents/GitHub/LandWeb/cache/MB_Free/* cache/MB_Free/ --delete
+rsync -avzh emcintir@132.156.148.172:/home/emcintir/Documents/GitHub/LandWeb/outputs/MB_Free/* outputs/MB_Free/ --delete
+rsync -avzh emcintir@132.156.148.172:/home/emcintir/Documents/GitHub/LandWeb/www/Free/MB/* www/Free/MB/ --delete
 
 
