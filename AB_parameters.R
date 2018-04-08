53d8b546b5c160020b5ce5d69138b9a5aade9660
Eliot McIntire <eliotmcintire@gmail.com>
2018-04-07 18:23:57 -0700

    use 3 clusters for AB

--- AB_parameters.R
+++ AB_parameters.R
@@ -1,6 +1,7 @@
-numClusters <- 4
+numClusters <- 3
 source("ThousandYrFreeProprietary.R")
 subStudyRegionName <- "AB"
 authenticationType <- list("Proprietary") # Can do one or both of "Free" "Proprietary"
 
 cacheIds4Experiment <- list("b846dd2c16298d02635b0b8ceb92354e")
+#cachdIds4ReportingAndLeadingFn <- "c7d5cbf8e786138158e7bdaf36bfd023"
