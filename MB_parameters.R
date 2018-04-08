7d3a170f35d08376aa5c1fef1b57952cbe39c8b6
Eliot McIntire <eliotmcintire@gmail.com>
2018-04-07 20:43:12 -0700

    MB

--- MB_parameters.R
+++ MB_parameters.R
@@ -11,5 +11,5 @@ cacheIdVtmRasters <- "30cd04ebab45e40cda8e44c0b96c69a7"
 cacheIdVtmsTifs <- "fb29ae26e6ae8b1909afcc1116803fc2"
 cachdId4ReportingAndLeadingFn <- "95a1e38ca60cb523d67b7d1ef3a82e09"
 cacheIdCurrentCondition <- "5ed7bc3c9e7866304678c47ed0cfae05"
-#cacheIdLrgPatchesCC
-#cacheIdLrgPatches
+cacheIdLrgPatchesCC <- "90a14691c0071e983476c0cb001a951a"
+cacheIdLrgPatches <- "1907907b3f3825df92e4c8a840783509"
