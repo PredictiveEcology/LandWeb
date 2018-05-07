f064c297b350e860b89e165f28ca193b76d05097
Alex Chubaty <alex.chubaty@gmail.com>
2018-05-07 13:33:29 -0700

    update 342 script

--- deploy/setup_342.sh
+++ deploy/setup_342.sh
@@ -5,9 +5,27 @@ mkdir -p /mnt/shared/LandWeb/www
 sudo groupadd landweb
 sudo usermod -a -G landweb achubaty
 sudo usermod -a -G landweb emcintir
-sudo chown :landweb /mnt/shared/
 sudo chown -R :landweb /mnt/shared/LandWeb
+sudo chmod -R g+w /mnt/shared/LandWeb
 
+################################################################################
+# do the following as eliot:
+sudo su emcintir
+
+## cache/
+mv /home/emcintir/Documents/GitHub/LandWeb/cache /mnt/shared/LandWeb/
 ln -s /mnt/shared/LandWeb/cache ~/Documents/GitHub/LandWeb/cache
+
+## outputs/
+mv /home/emcintir/Documents/GitHub/LandWeb/outputs /mnt/shared/LandWeb/
 ln -s /mnt/shared/LandWeb/outputs ~/Documents/GitHub/LandWeb/outputs
+
+## www/
+mv ~/Documents/GitHub/LandWeb/www /mnt/shared/LandWeb/
 ln -s /mnt/shared/LandWeb/www ~/Documents/GitHub/LandWeb/www
+
+exit
+################################################################################
+
+sudo chown -R :landweb /mnt/shared/LandWeb
+sudo chmod -R g+w /mnt/shared/LandWeb
