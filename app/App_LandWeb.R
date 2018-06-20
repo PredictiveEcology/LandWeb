1333745af0545b95945d1c32cb86369c3db61fd8
Alex Chubaty <alex.chubaty@gmail.com>
2018-06-20 16:57:31 -0700

    fixes to use updated SpaDES* packages

--- app/App_LandWeb.R
+++ app/App_LandWeb.R
@@ -3,10 +3,16 @@ if (packageVersion("shiny") > "1.0.5") {
   install.packages("https://cran.r-project.org/src/contrib/Archive/shiny/shiny_1.0.5.tar.gz", repos = NULL)
 }
 
+if (packageVersion("reproducible") < "0.2.0.9000") {
+  install_github("PredictiveEcology/reproducible@development")
+}
+
 # source("R/locateGdal.R")
 # source("R/updateSpaDES@developmentPkgs.R")
 # source("R/developmentParameters.R") # endTime, authenticationType
 # source("generateNewApp.R")
 source("params/LandWeb_parameters.R")
 file.copy("global_file.R", "global.R", overwrite = TRUE)
+
+load_all("~/Documents/GitHub/SpaDES.shiny")
 shiny::runApp(".", launch.browser = TRUE, port = 5921)
