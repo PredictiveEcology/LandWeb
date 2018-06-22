6e9ab52103fe7704d819dcffef6b3d8be27147a1
Alex Chubaty <alex.chubaty@gmail.com>
2018-06-22 13:15:25 -0700

    update pkg versions

--- app/App_LandWeb.R
+++ app/App_LandWeb.R
@@ -1,9 +1,4 @@
-## requires older version of shiny (see #32)
-if (packageVersion("shiny") > "1.0.5") {
-  install.packages("https://cran.r-project.org/src/contrib/Archive/shiny/shiny_1.0.5.tar.gz", repos = NULL)
-}
-
-if (packageVersion("reproducible") < "0.2.0.9000") {
+if (packageVersion("reproducible") < "0.2.0.9002") {
   install_github("PredictiveEcology/reproducible@development")
 }
 
@@ -13,6 +8,4 @@ if (packageVersion("reproducible") < "0.2.0.9000") {
 # source("generateNewApp.R")
 source("params/LandWeb_parameters.R")
 file.copy("global_file.R", "global.R", overwrite = TRUE)
-
-load_all("~/Documents/GitHub/SpaDES.shiny")
 shiny::runApp(".", launch.browser = TRUE, port = 5921)
