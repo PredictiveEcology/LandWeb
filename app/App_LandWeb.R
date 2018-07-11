b2a778e48b2dafbf4c3a444003ab3ab4cbc31770
Alex Chubaty <alex.chubaty@gmail.com>
2018-07-11 12:33:26 -0600

    use latest SpaDES.shiny
    with e522de72

--- app/App_LandWeb.R
+++ app/App_LandWeb.R
@@ -4,6 +4,9 @@ if (packageVersion("reproducible") < "0.2.1") {
 if (packageVersion("SpaDES.core") < "0.2.0") {
   install_github("PredictiveEcology/SpaDES.core@development")
 }
+if (packageVersion("SpaDES.shiny") < "0.1.0.9006") {
+  install_github("PredictiveEcology/SpaDES.core@development")
+}
 
 #source("params/LandWeb_parameters.R") # moved to global.R
 file.copy("global_file.R", "global.R", overwrite = TRUE)
