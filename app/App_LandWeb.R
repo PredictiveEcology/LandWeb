f7f181fc93234c8e7e4de911d5031af87410ee98
Alex Chubaty <alex.chubaty@gmail.com>
2018-08-08 14:14:22 -0600

    use latest SpaDES.shiny (0.1.0.9009)

--- app/App_LandWeb.R
+++ app/App_LandWeb.R
@@ -4,7 +4,7 @@ if (packageVersion("reproducible") < "0.2.1") {
 if (packageVersion("SpaDES.core") < "0.2.0") {
   install_github("PredictiveEcology/SpaDES.core@development")
 }
-if (packageVersion("SpaDES.shiny") < "0.1.0.9008") {
+if (packageVersion("SpaDES.shiny") < "0.1.0.9009") {
   install_github("PredictiveEcology/SpaDES.shiny@development")
 }
 
