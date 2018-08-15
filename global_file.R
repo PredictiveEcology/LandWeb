d942f45219ddb715c8fbff695592af3d39581171
Eliot McIntire <eliotmcintire@gmail.com>
2018-08-15 08:33:59 -0700

    addResourcePath needs path to exist

--- global_file.R
+++ global_file.R
@@ -43,6 +43,9 @@ reproducible::Require(unique(c(
 )))
 packageLoadEndTime <- Sys.time()
 
+checkPath(file.path("www/All/FULL/map-tiles"), create = TRUE)
+shiny::addResourcePath("tiles", "www/All/FULL/map-tiles")
+
 ## LandWeb app information
 source("appInfo.R")
 
