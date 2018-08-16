476cef821e4d2c4a9d458a18360eb32f21f571d1
Alex Chubaty <alex.chubaty@gmail.com>
2018-08-16 15:14:48 -0600

    modularize recalc

--- app/Development_App.R
+++ app/Development_App.R
@@ -7,6 +7,8 @@ if (!require("profvis", character.only = TRUE, quietly = TRUE)) {
 DEVMODE <- TRUE
 PROFVIS <- FALSE
 
+useCache342 <- TRUE ## set this to FALSE to run model from scratch
+
 #source("params/Development_Parameters.R") # moved to global.R
 file.copy("global_file.R", "global.R", overwrite = TRUE)
 if (exists("PROFVIS") && isTRUE(PROFVIS)) {
