93ada9cca1eeb1b150ab58e3feec965980030a0a
Alex Chubaty <alex.chubaty@gmail.com>
2018-08-23 15:19:44 -0600

    don't use cache using development app

--- app/Development_App.R
+++ app/Development_App.R
@@ -7,7 +7,7 @@ if (!require("profvis", character.only = TRUE, quietly = TRUE)) {
 DEVMODE <- TRUE
 PROFVIS <- FALSE
 
-useCache342 <- TRUE ## set this to FALSE to run model from scratch
+useCache342 <- FALSE ## set this to FALSE to run model from scratch
 
 #source("params/Development_Parameters.R") # moved to global.R
 file.copy("global_file.R", "global.R", overwrite = TRUE)
