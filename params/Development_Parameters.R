3ab01ac395156c06732532797279a238568ca1f5
Alex Chubaty <alex.chubaty@gmail.com>
2018-08-23 19:09:11 -0600

    cleanup

--- params/Development_Parameters.R
+++ params/Development_Parameters.R
@@ -6,7 +6,6 @@ useParallelCluster <- FALSE
 cl6 <- NULL
 lapplyFn <- "lapply"
 
-
 # cacheId for 1000 years: 2e35699c4ade1b4bfa82e864558c7436, 7.3 days - on 342
 authenticationType <- list("Free", "Proprietary") # Can do one or both of "Free" "Proprietary"
 # Spatial stuff -- determines the size of the area that will be "run" in the simulations
