dc18ef94e8d2354125b3c53464c9c836af0e35e2
Eliot McIntire <eliotmcintire@gmail.com>
2018-10-19 14:49:40 -0700

    github_install new version of reproducible

--- R/updateSpaDES@developmentPkgs.R
+++ R/updateSpaDES@developmentPkgs.R
@@ -1,5 +1,5 @@
 devtools::install_github("PredictiveEcology/quickPlot", ref = "development", dependencies = FALSE)
-devtools::install_github("PredictiveEcology/reproducible", ref = "development", dependencies = FALSE)
+devtools::install_github("PredictiveEcology/reproducible", ref = "forMapObject", dependencies = FALSE)
 devtools::install_github("PredictiveEcology/webDatabases", ref = "master", dependencies = FALSE)
 devtools::install_github("PredictiveEcology/SpaDES.tools", ref = "development", dependencies = FALSE)
 devtools::install_github("PredictiveEcology/SpaDES.core", ref = "newSetClass", dependencies = FALSE)
