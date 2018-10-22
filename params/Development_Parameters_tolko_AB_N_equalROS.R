0d0efab4ead08aa40d2237326577bdef3f93f202
Alex Chubaty <alex.chubaty@gmail.com>
2018-10-21 23:32:29 -0600

    update run params etc

--- params/Development_Parameters_tolko_AB_N_equalROS.R
+++ params/Development_Parameters_tolko_AB_N_equalROS.R
@@ -1,6 +1,6 @@
 # Overall model times # start is default at 0
-endTime <- 1500
+endTime <- 900
 successionTimestep <- 10
 summaryPeriod <- c(700, endTime)
-summaryInterval <- 20
+summaryInterval <- 100
 cacheId <- list()
