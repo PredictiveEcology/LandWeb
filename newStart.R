1bc2aa2cd083f699b6e7157d866e1bf88631cce0
Alex Chubaty <alex.chubaty@gmail.com>
2018-10-23 12:14:17 -0600

    add runs for  log(ROS)

--- newStart.R
+++ newStart.R
@@ -18,6 +18,11 @@ runName <- "testing"
 #runName <- "tolko_AB_S_equalROS"
 #runName <- "tolko_SK_equalROS"
 
+
+#runName <- "tolko_AB_N_logROS"
+#runName <- "tolko_AB_S_logROS"
+#runName <- "tolko_SK_logROS"
+
 source(file.path("params", paste0("Development_Parameters_", runName, ".R")))
 
 ##########################################################
