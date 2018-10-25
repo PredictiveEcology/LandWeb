efdcd3112aac525385bd13f39c487eef0b7bf63d
Alex Chubaty <alex.chubaty@gmail.com>
2018-10-25 17:41:21 -0600

    rerun original AB_S

--- params/Development_Parameters_tolko_AB_S_doubleFRI.R
+++ params/Development_Parameters_tolko_AB_S_doubleFRI.R
@@ -1,6 +1,6 @@
 # Overall model times # start is default at 0
-endTime <- 900
+endTime <- 700
 successionTimestep <- 10
-summaryPeriod <- c(700, endTime)
-summaryInterval <- 100
+summaryPeriod <- c(500, endTime)
+summaryInterval <- 25
 cacheId <- list()
