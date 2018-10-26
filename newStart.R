341628714d63ffaf0d9631860711104e9e92467a
Alex Chubaty <alex.chubaty@gmail.com>
2018-10-26 08:07:09 -0700

    update run status

--- newStart.R
+++ newStart.R
@@ -19,7 +19,7 @@ runName <- "testing"
 #runName <- "tolko_SK_equalROS" ## DONE
 
 ## running on 388
-#runName <- "tolko_AB_N_logROS"
+#runName <- "tolko_AB_N_logROS" ## done but had an error
 #runName <- "tolko_AB_S_logROS" ## done but no caribou, ansr
 #runName <- "tolko_SK_logROS" ## DONE
 
