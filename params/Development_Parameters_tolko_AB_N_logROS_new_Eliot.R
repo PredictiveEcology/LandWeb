50a6852dbb2d566c5e8f09e57cbfac12fd16c613
Eliot McIntire <eliotmcintire@gmail.com>
2018-11-05 16:33:57 -0800

    parameters tweaks for Eliot's stuff

--- params/Development_Parameters_tolko_AB_N_logROS_new_Eliot.R
+++ params/Development_Parameters_tolko_AB_N_logROS_new_Eliot.R
@@ -1,6 +1,6 @@
 # Overall model times # start is default at 0
 endTime <- 900
-successionTimestep <- 10
+successionTimestep <- 2
 summaryPeriod <- c(700, endTime)
 summaryInterval <- 100
 cacheId <- list()
