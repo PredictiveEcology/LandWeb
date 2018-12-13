c3b0c892b8e24150a62414562d08e48aa8220f43
Eliot McIntire <eliotmcintire@gmail.com>
2018-12-13 15:51:14 -0800

    Major update to rasterToMatch -- ONLY forest

--- m/LandWeb_preamble/LandWeb_preamble.R
+++ m/LandWeb_preamble/LandWeb_preamble.R
@@ -33,7 +33,7 @@ defineModule(sim, list(
     createsOutput("fireReturnInterval", "RasterLayer", desc = NA),
     createsOutput("LandTypeCC", "RasterLayer", desc = NA),
     createsOutput("LCC2005", "RasterLayer", desc = NA),
-    createsOutput("nonVegPixels", "integer", desc = NA),
+    createsOutput("nonTreePixels", "integer", desc = NA),
     createsOutput("rasterToMatch", "RasterLayer", desc = NA),
     createsOutput("rasterToMatchReporting", "RasterLayer", desc = NA),
     createsOutput("rstFlammable", "RasterLayer", desc = NA),
@@ -188,23 +188,82 @@ Init <- function(sim) {
   }
 
   ##########################################################
-  # Current Conditions (Age Map)
-  ##########################################################
-  ccURL <- "https://drive.google.com/file/d/1JnKeXrw0U9LmrZpixCDooIm62qiv4_G1/view?usp=sharing"
-
-  fname_age <- "Age1.tif"
-  ml <- mapAdd(map = ml, url = ccURL, layerName = "CC TSF", CC = TRUE,
-               tsf = file.path(Paths$inputPath, fname_age), analysisGroup1 = "CC",
-               targetFile = fname_age, filename2 = NULL,
-               useCache = TRUE, isRasterToMatch = TRUE,
-               alsoExtract = "similar", leaflet = FALSE)
-
-  ##########################################################
   # LCC2005
   ##########################################################
   LCC2005 <- pemisc::prepInputsLCC(studyArea = studyArea(ml), destinationPath = Paths$inputPath)
   ml <- mapAdd(LCC2005, layerName = "LCC2005", map = ml, filename2 = NULL, leaflet = FALSE,
-               isRasterToMatch = FALSE, method = "ngb")
+               isRasterToMatch = TRUE, method = "ngb")
+
+  ##########################################################
+  # Current Conditions
+  ##########################################################
+  ccURL <- "https://drive.google.com/file/d/1JnKeXrw0U9LmrZpixCDooIm62qiv4_G1/view?usp=sharing"
+  LandTypeFileCC <- file.path(Paths$inputPath, "LandType1.tif")
+  sim$LandTypeCC <- Cache(prepInputs, LandTypeFileCC, studyArea = studyArea(ml),
+                          url = ccURL, method = "ngb",
+                          rasterToMatch = rasterToMatch(ml), filename2 = NULL)
+
+
+  ################
+  # Non Tree pixels
+  ##################
+  # Setting NA values
+  # 3 is shrub, wetland, grassland -- no veg dynamics happen -- will burn in fire modules
+  # 4 is water, rock, ice
+  # 5 is no Data ... this is currently cropland -- will be treated as grassland for fires
+  treeClassesCC <- c(0, 1, 2)
+  nontreeClassesCC <- c(3, 4)
+  treePixelsTF <- sim$LandTypeCC[] %in% treeClassesCC
+  #nonTreePixels <- sim$LandTypeCC[] %in% nontreeClassesCC
+
+  treeClassesLCC <- c(1:15, 34:35)
+  treePixelsLCCTF <- ml$LCC2005[] %in% treeClassesLCC
+
+  noDataPixels <- is.na(sim$LandTypeCC[]) | sim$LandTypeCC[] == 5
+  noDataPixelsLCC <- is.na(ml$LCC2005[]) | ml$LCC2005[] == 0
+
+  treePixels <- which(treePixelsTF)
+  treePixelsLCCTF[!noDataPixels] <- NA
+  treePixelsLCC <- which(treePixelsLCCTF)
+
+  treePixelsCombined <- unique(c(treePixels, treePixelsLCC))
+  nonTreePixels <- seq(ncell(ml$LCC2005))
+  nonTreePixels <- nonTreePixels[!nonTreePixels %in% treePixelsCombined]
+
+  sim$nonTreePixels <- nonTreePixels
+
+  # Update rasterToMatch layer with all trees
+  ml[[ml@metadata[ml@metadata$rasterToMatch == 1, ]$layerName]][sim$nonTreePixels] <- NA
+
+  fname_age <- "Age1.tif"
+  TSFLayerName <- "CC TSF"
+  ml <- mapAdd(map = ml, url = ccURL, layerName = TSFLayerName, CC = TRUE,
+               tsf = file.path(Paths$inputPath, fname_age), analysisGroup1 = "CC",
+               targetFile = fname_age, filename2 = NULL,
+               useCache = TRUE, isRasterToMatch = FALSE,
+               alsoExtract = "similar", leaflet = FALSE)
+
+
+  ########################################################################
+  # Age from KNN
+  ########################################################################
+
+  standAgeMapFilename <- "NFI_MODIS250m_kNN_Structure_Stand_Age_v0.tif"
+  standAgeMap <- Cache(prepInputs, #notOlderThan = Sys.time(),
+                       targetFile = standAgeMapFilename,
+                       archive = asPath(c("kNN-StructureStandVolume.tar",
+                                          "NFI_MODIS250m_kNN_Structure_Stand_Age_v0.zip")),
+                       destinationPath = Paths$inputPath,
+                       url = "http://tree.pfc.forestry.ca/kNN-StructureStandVolume.tar",
+                       fun = "raster::raster",
+                       studyArea = studyArea(ml),
+                       rasterToMatch = rasterToMatch(ml),
+                       maskWithRTM = TRUE,
+                       method = "bilinear",
+                       datatype = "INT2U",
+                       filename2 = NULL, overwrite = TRUE,
+                       userTags = c("stable", currentModule(sim)))
+  ml[[TSFLayerName]][noDataPixels] <- standAgeMap[noDataPixels]
 
   ##########################################################
   # Clean up the study area
@@ -219,31 +278,27 @@ Init <- function(sim) {
   #    NAs outside the buffered study & snow/rock/ice area
   #    the only values we want NA
   #    use the LCC flammability map to fill in NA / nodata values
-  LandTypeFileCC <- file.path(Paths$inputPath, "LandType1.tif")
-  sim$LandTypeCC <- Cache(prepInputs, LandTypeFileCC, studyArea = studyArea(ml),
-                      url = ccURL, method = "ngb",
-                      rasterToMatch = rasterToMatch(ml), filename2 = NULL)
 
   # No data class is 5 -- these will be filled in by LCC2005 layer
-  NA_ids <- which(is.na(sim$LandTypeCC[]) | sim$LandTypeCC[] == 5)
+  # NA_ids <- which(is.na(sim$LandTypeCC[]) | sim$LandTypeCC[] == 5)
   # Only class 4 is considered non-flammable
   rstFlammableCC <- defineFlammable(sim$LandTypeCC, nonFlammClasses = 4,
                                     mask = NULL, filename2 = NULL)
 
   LandTypeFileLCC <- file.path(Paths$inputPath, "LCC2005_V1_4a.tif")
   # Only classes 36, 37, 38, 39 is considered non-flammable
-  rstFlammableLCC <- Cache(prepInputs, LandTypeFileLCC, studyArea = studyArea(ml),
-                           url = ccURL, method = "ngb",
-                           rasterToMatch = rasterToMatch(ml), filename2 = NULL) %>%
-    defineFlammable(., nonFlammClasses = c(36, 37, 38, 39), mask = NULL, filename2 = NULL)
+  rstFlammableLCC <- #Cache(prepInputs, LandTypeFileLCC, studyArea = studyArea(ml),
+                    #       url = ccURL, method = "ngb",
+                    #       rasterToMatch = rasterToMatch(ml), filename2 = NULL) %>%
+    defineFlammable(ml$LCC2005, nonFlammClasses = c(36, 37, 38, 39), mask = NULL, filename2 = NULL)
 
   sim$rstFlammable <- rstFlammableCC
-  sim$rstFlammable[NA_ids] <- rstFlammableLCC[NA_ids]
+  sim$rstFlammable[noDataPixels] <- rstFlammableLCC[noDataPixels]
 
   ## fireReturnInterval needs to be masked by rstFlammable
   rstFireReturnInterval <- fasterize::fasterize(sf::st_as_sf(studyArea(ml)),
                                                 raster = rasterToMatch(ml),
-                              field = "fireReturnInterval")
+                                                field = "fireReturnInterval")
   #rtm <- rasterToMatch(studyArea(ml), rasterToMatch = rasterToMatch(ml))
   #rstFireReturnInterval <- Cache(postProcess, rtm, maskvalue = 0L, filename2 = NULL)
   ml <- mapAdd(rstFireReturnInterval, layerName = "fireReturnInterval", filename2 = NULL,
@@ -258,33 +313,23 @@ Init <- function(sim) {
 
   #ml$fireReturnInterval <- raster(ml$fireReturnInterval) # blank out values for new, non-factor version
   #ml$fireReturnInterval[] <- fireReturnInterval
-  ml@metadata[layerName == "LCC2005", rasterToMatch := NA]
-
-  # Setting NA values
-  # 3 is shrub, wetland, grassland -- no veg dynamics happen -- will burn in fire modules
-  # 4 is water, rock, ice
-  # 5 is no Data ... this is currently cropland -- will be treated as grassland for fires
-  nonVegPixels <- sim$LandTypeCC[] %in% c(3,4)
-
-  NApixels <- is.na(sim$LandTypeCC[]) | sim$LandTypeCC[] == 5
-
-  nonVegPixels[NApixels] <- (sim$LCC2005[] %in% c(16:33, 36:39))[NApixels]
-
-  sim$nonVegPixels <- which(nonVegPixels)
+  # ml@metadata[layerName == "LCC2005", rasterToMatch := NA]
 
   sim$studyArea <- studyArea(ml, 3)
   sim$studyAreaLarge <- studyArea(ml, 1)
   sim$studyAreaReporting <- studyArea(ml, 2)
   sim$rasterToMatch <- rasterToMatch(ml)
-  sim$rasterToMatch[sim$nonVegPixels] <- NA
+  #sim$rasterToMatch[sim$nonTreePixels] <- NA
 
   sim$fireReturnInterval <- ml$fireReturnInterval # no NAing here because this needs only
 
   sim$LCC2005 <- ml$LCC2005
-  sim$LCC2005[sim$nonVegPixels] <- NA
+  #sim$LCC2005[sim$nonTreePixels] <- NA
+  #LCC2005 <- sim$LCC2005
+  #LCC2005[sim$nonTreePixels] <- NA
 
-  sim$`CC TSF` <- ml$`CC TSF`
-  sim$`CC TSF`[sim$nonVegPixels] <- NA
+  sim[[TSFLayerName]] <- ml[[TSFLayerName]]
+  #sim$`CC TSF`[sim$nonTreePixels] <- NA
 
   sim$rasterToMatchReporting <- postProcess(rasterToMatch(ml),
                                             studyArea = studyArea(ml, 2),
