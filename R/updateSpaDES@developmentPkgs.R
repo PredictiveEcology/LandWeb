577901f2b839e194c2497c49e8212a084cd69b3e
Alex Chubaty <alex.chubaty@gmail.com>
2018-12-31 14:14:58 -0700

    use quickPlot@development

--- R/updateSpaDES@developmentPkgs.R
+++ R/updateSpaDES@developmentPkgs.R
@@ -1,6 +1,6 @@
 #git clone git@github.com:eliotmcintire/LandWeb --branch rewriteNoAll --recurse-submodules -j8
 
-#devtools::install_github("PredictiveEcology/quickPlot", ref = "master", dependencies = FALSE)
+#devtools::install_github("PredictiveEcology/quickPlot", ref = "development", dependencies = FALSE)
 devtools::install_github("PredictiveEcology/reproducible", ref = "development", dependencies = FALSE)
 #devtools::install_github("PredictiveEcology/webDatabases", ref = "master", dependencies = FALSE)
 devtools::install_github("PredictiveEcology/SpaDES.tools", ref = "development", dependencies = FALSE)
