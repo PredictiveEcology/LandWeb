cd9315baf1c41ce388a2aeb208b423430871ec9b
Alex Chubaty <alex.chubaty@gmail.com>
2019-02-19 16:23:07 -0700

    cleanup and tweaks

--- params/scfm_params.R
+++ params/scfm_params.R
@@ -1,32 +1,17 @@
-mapDim <- 200
 defaultInterval <- NA
 defaultPlotInterval <- NA
 defaultInitialSaveTime <- NA
 
-scfmModules <- list("andisonDriver_dataPrep", "andisonDriver", "scfmLandcoverInit",
-                    "scfmIgnition", "ageModule", "scfmRegime", "scfmEscape", "scfmSpread")
-
-scfmObjects <- list("mapDim" = mapDim)
-
 scfmParams <- list(
   #.progress = list(type = "text", interval = 1),
-  ageModule = list(
-    initialAge = 100,
-    maxAge = 200,
-    returnInterval = defaultInterval,
-    startTime = times$start,
-    .plotInitialTime = times$start,
-    .plotInterval = defaultPlotInterval,
-    .saveInitialTime = defaultInitialSaveTime,
-    .saveInterval = defaultInterval),
   scfmIgnition = list(
     pIgnition = 0.0001,
-    returnInterval = defaultInterval,
+    returnInterval = 1, ## annual fires
     startTime = times$start,
     .plotInitialTime = NA,
-    .plotInterval = defaultPlotInterval,
-    .saveInitialTime = defaultInitialSaveTime,
-    .saveInterval = defaultInterval),
+    .plotInterval = NA,
+    .saveInitialTime = NA,
+    .saveInterval = NA),
   scfmEscape = list(
     p0 = 0.05,
     returnInterval = defaultInterval,
