607026eac657db675ca12708a25efe7535d2016a
Alex Chubaty <alex.chubaty@gmail.com>
2019-02-26 14:39:33 -0700

    batchMode

---
+++ batch_runs.R
@@ -0,0 +1,1 @@
+1 -> rep; Sys.sleep(100*rep); runName <- paste0("DMI_aspenDispersal_logROS_run", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R")
--- newStart.R
+++ newStart.R
@@ -3,6 +3,7 @@ quickPlot::dev.useRSGD(useRSGD = FALSE) ## TODO: temporary for Alex's testing
 activeDir <- if (pemisc::user("rstudio")) "~/LandWeb" else "~/GitHub/LandWeb"
 ageClasses <- c("Young", "Immature", "Mature", "Old")
 ageClassCutOffs <- c(0, 40, 80, 120)
+batchMode <- TRUE ## NOTE: runName must be defined
 cloudCacheFolderID <- "/folders/1ry2ukXeVwj5CKEmBW1SZVS_W8d-KtmIj"
 eventCaching <- c(".inputObjects", "init")
 fireTimestep <- 1
@@ -26,67 +27,70 @@ setwd(activeDir)
 ## set run name
 ##############################################################
 
-if (pemisc::user("achubaty")) runName <- "tolko_SK_aspenDispersal_logROS_test01"
+if (isTRUE(batchMode)) {
+  stopifnot(exists("runName", envir = .GlobalEnv)) ## run name should be set in batch_mode.R
+} else {
+  if (pemisc::user("achubaty")) runName <- "tolko_SK_aspenDispersal_logROS_test01"
 
-## running locally
-#runName <- "ANC"
-#runName <- "ANC_aspenDispersal_logROS"
-#runName <- "ANC_doubleFRI"
-#runName <- "ANC_equalROS"
-#runName <- "ANC_logROS"
-#runName <- "ANC_noDispersal"
+  ## running locally
+  #runName <- "ANC"
+  #runName <- "ANC_aspenDispersal_logROS"
+  #runName <- "ANC_doubleFRI"
+  #runName <- "ANC_equalROS"
+  #runName <- "ANC_logROS"
+  #runName <- "ANC_noDispersal"
 
-## running locally
-#runName <- "DMI"
-#runName <- "DMI_aspenDispersal_logROS"
-#runName <- "DMI_doubleFRI"
-#runName <- "DMI_equalROS"
-#runName <- "DMI_logROS"
-#runName <- "DMI_noDispersal"
+  ## running locally
+  #runName <- "DMI"
+  #runName <- "DMI_aspenDispersal_logROS"
+  #runName <- "DMI_doubleFRI"
+  #runName <- "DMI_equalROS"
+  #runName <- "DMI_logROS"
+  #runName <- "DMI_noDispersal"
 
-## running locally
-#runName <- "LP_MB"
-#runName <- "LP_MB_aspenDispersal_logROS"
-#runName <- "LP_MB_doubleFRI"
-#runName <- "LP_MB_equalROS"
-#runName <- "LP_MB_logROS"
-#runName <- "LP_MB_noDispersal"
+  ## running locally
+  #runName <- "LP_MB"
+  #runName <- "LP_MB_aspenDispersal_logROS"
+  #runName <- "LP_MB_doubleFRI"
+  #runName <- "LP_MB_equalROS"
+  #runName <- "LP_MB_logROS"
+  #runName <- "LP_MB_noDispersal"
 
-## running locally
-#runName <- "tolko_AB_N"
-#runName <- "tolko_AB_S"
-#runName <- "tolko_SK"
+  ## running locally
+  #runName <- "tolko_AB_N"
+  #runName <- "tolko_AB_S"
+  #runName <- "tolko_SK"
 
-## running locally
-#runName <- "tolko_AB_N_doubleFRI"
-#runName <- "tolko_AB_S_doubleFRI"
-#runName <- "tolko_SK_doubleFRI"
+  ## running locally
+  #runName <- "tolko_AB_N_doubleFRI"
+  #runName <- "tolko_AB_S_doubleFRI"
+  #runName <- "tolko_SK_doubleFRI"
 
-## running locally
-#runName <- "tolko_AB_N_equalROS"
-#runName <- "tolko_AB_S_equalROS"
-#runName <- "tolko_SK_equalROS"
+  ## running locally
+  #runName <- "tolko_AB_N_equalROS"
+  #runName <- "tolko_AB_S_equalROS"
+  #runName <- "tolko_SK_equalROS"
 
-## running locally
-# runName <- "tolko_AB_N_logROS"
-#runName <- "tolko_AB_S_logROS"
-if (pemisc::user("emcintir")) runName <- "tolko_SK_logROS"
+  ## running locally
+  # runName <- "tolko_AB_N_logROS"
+  #runName <- "tolko_AB_S_logROS"
+  if (pemisc::user("emcintir")) runName <- "tolko_SK_logROS"
 
-## running locally
-#runName <- "tolko_AB_N_noDispersal"
-#runName <- "tolko_AB_S_noDispersal"
-#runName <- "tolko_SK_noDispersal"
+  ## running locally
+  #runName <- "tolko_AB_N_noDispersal"
+  #runName <- "tolko_AB_S_noDispersal"
+  #runName <- "tolko_SK_noDispersal"
 
-## running locally
-#runName <- "tolko_AB_N_aspenDispersal_logROS"
-#runName <- "tolko_AB_S_aspenDispersal_logROS"
-#runName <- "tolko_SK_aspenDispersal_logROS"
+  ## running locally
+  #runName <- "tolko_AB_N_aspenDispersal_logROS"
+  #runName <- "tolko_AB_S_aspenDispersal_logROS"
+  #runName <- "tolko_SK_aspenDispersal_logROS"
 
-## running locally
-#runName <- "tolko_AB_N_aspen80"
-#runName <- "tolko_AB_S_aspen80"
-#runName <- "tolko_SK_aspen80"
-
+  ## running locally
+  #runName <- "tolko_AB_N_aspen80"
+  #runName <- "tolko_AB_S_aspen80"
+  #runName <- "tolko_SK_aspen80"
+}
 message(crayon::red(runName))
 
 if (grepl("LandWeb", runName)) {
