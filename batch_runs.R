cc459e0c30ea0ade3556e704f4aee07ad3b6f9e0
Alex Chubaty <alex.chubaty@gmail.com>
2019-02-27 16:20:36 -0700

    exit after batch_runs

--- batch_runs.R
+++ batch_runs.R
@@ -1,1 +1,1 @@
-1 -> rep; Sys.sleep(600*rep); runName <- paste0("DMI_aspenDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R")
+1 -> rep; Sys.sleep(600*rep); runName <- paste0("DMI_aspenDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R"); Q()
