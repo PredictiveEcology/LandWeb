a75a00c1279838c7e5feba11190c2ca14c0f5641
Alex Chubaty <alex.chubaty@gmail.com>
2019-03-04 16:08:16 -0700

    update batch runs

--- batch_runs.R
+++ batch_runs.R
@@ -1,9 +1,11 @@
-1 -> rep; Sys.sleep(600*rep); runName <- paste0("tolko_AB_N_aspenDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R"); Q()
+1 -> rep; Sys.sleep(600*rep); runName <- paste0("DMI_aspenDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R"); Q()
+
+1 -> rep; Sys.sleep(20*3600 + 600*rep); runName <- paste0("tolko_AB_N_aspenDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R"); Q()
 
 1 -> rep; Sys.sleep(36*3600 + 600*rep); runName <- paste0("tolko_AB_S_aspenDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R"); Q()
 
-1 -> rep; Sys.sleep(72*3600 + 600*rep); runName <- paste0("tolko_SK_aspenDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R"); Q()
+1 -> rep; Sys.sleep(50*3600 + 600*rep); runName <- paste0("tolko_SK_aspenDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R"); Q()
 
-1 -> rep; Sys.sleep(24*3600 + 600*rep); runName <- paste0("ANC_aspenDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R"); Q()
+1 -> rep; Sys.sleep(60*3600 + 600*rep); runName <- paste0("ANC_aspenDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R"); Q()
 
-1 -> rep; Sys.sleep(12*3600 + 600*rep); runName <- paste0("LP_MB_aspenDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R"); Q()
+1 -> rep; Sys.sleep(72*3600 + 600*rep); runName <- paste0("LP_MB_aspenDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R"); Q()
