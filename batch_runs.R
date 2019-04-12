207ca423db3648ba1105c9ed0e28865b52b38d3c
Alex Chubaty <alex.chubaty@gmail.com>
2019-04-12 09:51:54 -0600

    add LandWeb batch runs
    TODO: estimate time between runs and RAM used per run so we don't over-use the machine

--- batch_runs.R
+++ batch_runs.R
@@ -10,3 +10,7 @@
 1 -> rep; Sys.sleep(60*3600 + 600*rep); runName <- paste0("ANC_aspenDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R"); print(runName); Q()
 
 1 -> rep; Sys.sleep(72*3600 + 600*rep); runName <- paste0("LP_MB_aspenDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R"); print(runName); Q()
+
+#####
+
+1 -> rep; Sys.sleep(600*rep); runName <- paste0("LandWeb_aspenDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R"); print(runName); Q()
