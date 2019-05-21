55a841685f1715ee778aa98b26d7619bd01c0d09
Eliot McIntire <eliotmcintire@gmail.com>
2019-05-21 16:48:02 -0700

    rm LandR_BiomassGMOrig

--- newStart.R
+++ newStart.R
@@ -356,7 +356,8 @@ tilePath <- file.path(Paths$outputPath, "tiles")
 
 if (isFALSE(postProcessOnly)) {
   times <- list(start = 0, end = endTime)
-  modules <- list("Boreal_LBMRDataPrep", "LandR_BiomassGMOrig", "LBMR",
+  modules <- list("Boreal_LBMRDataPrep", #"LandR_BiomassGMOrig",
+                  "LBMR",
                   "LandMine", "Biomass_regeneration",
                   "LandWeb_output",
                   "timeSinceFire")
@@ -433,9 +434,9 @@ if (isFALSE(postProcessOnly)) {
       ".useCache" = eventCaching[1], # seems slower to use Cache for both
       ".useParallel" = useParallel
     ),
-    LandR_BiomassGMOrig = list(
-      ".useParallel" = useParallel
-    ),
+    # LandR_BiomassGMOrig = list(
+    #   ".useParallel" = useParallel
+    # ),
     Biomass_regeneration = list(
       "fireInitialTime" = fireTimestep,
       "fireTimestep" = fireTimestep,
