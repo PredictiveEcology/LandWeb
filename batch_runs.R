befee08900e09ee3828553c7fa0d87a9a97705d5
Alex Chubaty <alex.chubaty@gmail.com>
2019-08-02 15:15:10 -0600

    manning batch run

--- batch_runs.R
+++ batch_runs.R
@@ -15,7 +15,7 @@
 
 1 -> rep; Sys.sleep(rep); runName <- paste0("tolko_SK_highDispersal_logROS_test_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); .starttime <- Sys.time(); source("newStart.R"); print(runName); Q()
 
-1 -> rep; Sys.sleep(600*rep); runName <- paste0("Manning_highDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R"); print(runName); Q()
+1 -> rep; Sys.sleep(rep); runName <- paste0("Manning_highDispersal_logROS_rep", SpaDES.core::paddedFloatToChar(rep, padL = 2)); source("newStart.R"); print(runName); Q()
 
 #####
 
