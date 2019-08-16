3fa6ae2ad6ccc0bffc572e1250524843ed3b3522
Alex Chubaty <alex.chubaty@gmail.com>
2019-08-16 14:20:06 -0600

    moduleDiagram's and objectDiagram's

--- newStart.R
+++ newStart.R
@@ -860,6 +860,19 @@ if (isFALSE(postProcessOnly)) {
                         .plotInitialTime = .plotInitialTime
       )
       saveRDS(mySimOut, file.path(Paths$outputPath, "mySimOut.rds"))
+      #mySimOut <- readRDS(file.path(Paths$outputPath, "mySimOut.rds"))
+
+      png(file.path("docs", "LandWeb_module_diagram.png"), height = 800, width = 800)
+        moduleDiagram(mySimOut)
+      dev.off()
+
+      ## currently not working: see https://github.com/rich-iannone/DiagrammeR/issues/207
+      ## workaround is to use Rstudio's manual export ability :S
+      objectDiagram(mySimOut, height = 2500, width = 1250) # %>%
+      #   DiagrammeRsvg::export_svg() %>%
+      #   charToRaw() %>%
+      #   rsvg::rsvg() %>%
+      #   png::writePNG(file.path("docs", "LandWeb_object_diagram.png"))
 
       if (grepl("LandWeb", runName)) {
         nRestarts <- ceiling(endTime / restartInterval)
