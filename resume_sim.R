bfc253986232f459ee8e770e8072b2b9d8cffaa0
Alex Chubaty <alex.chubaty@gmail.com>
2019-08-16 17:42:46 -0600

    explicitly reload packages
    not all packages were being reloaded (e.g., `VGAM`, `SpaDES.tools`)

--- resume_sim.R
+++ resume_sim.R
@@ -1,6 +1,10 @@
+Require(packages(mySimOut))
+
 restartIteration <- restartIteration + 1
 end(mySimOut) <- min(restartIteration * restartInterval, endTime)
+
 mySimOut <- spades(mySimOut)
+
 simFile <- file.path(Paths$outputPath, 'mySimOut.rds')
 saveRDS(mySimOut, simFile)
 message("Saving simulation to: ", simFile)
