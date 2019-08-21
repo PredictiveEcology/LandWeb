9a3f1194f32a135aef5289db323d0cf1060c0b59
Alex Chubaty <alex.chubaty@gmail.com>
2019-08-21 15:41:11 -0600

    module/object diagrams

--- 11-post-sim.R
+++ 11-post-sim.R
@@ -2,17 +2,33 @@
 ## post-simulation object creation (for documentation)
 ################################################################################
 
-#mySimOut <- readRDS(simFile("mySimOut", Paths$outputPath, NULL))
+modules_landweb <- c("LandWeb_preamble", "BiomassSpeciesData", modules3)
+
+fsim <- simFile("mySim_landweb", "docs", 0)
+mySim_landweb <- if (file.exists(fsim)) {
+  Cache(simInit,
+        times = times3,
+        params = parameters3,
+        modules = modules_landweb,
+        outputs = outputs3,
+        objects = objects3,
+        paths = paths3,
+        loadOrder = unlist(modules_landweb),
+        omitArgs = c("debug", "paths", ".plotInitialTime")
+  )
+  saveRDS(mySim_landweb, fsim)
+} else {
+  readRDS(fsim)
+}
 
 png(file.path("docs", "LandWeb_module_diagram.png"), height = 800, width = 800)
-  moduleDiagram(mySimOut)
+  moduleDiagram(mySim_landweb)
 dev.off()
 
 ## currently not working: see https://github.com/rich-iannone/DiagrammeR/issues/207
 ## workaround is to use Rstudio's manual export ability :S
-objectDiagram(mySimOut, height = 2500, width = 1250) # %>%
+objectDiagram(mySim_landweb, height = 2500, width = 1250) # %>%
 #   DiagrammeRsvg::export_svg() %>%
 #   charToRaw() %>%
 #   rsvg::rsvg() %>%
 #   png::writePNG(file.path("docs", "LandWeb_object_diagram.png"))
-
