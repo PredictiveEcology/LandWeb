d06816ed7c0000914f7fcb3aa06d31a85c2d5909
Alex Chubaty <alex.chubaty@gmail.com>
2020-02-07 11:31:59 -0700

    run_fma.sh tweaks (maintain backwards compatibility)

--- run_fma.sh
+++ run_fma.sh
@@ -6,7 +6,8 @@ FMA=$1
 RES=250
 printf -v RUN "%02g" $2 ## assign to RUN, padding with extra zeros as needed
 
-OUTDIR="outputs/${FMA}_res${RES}"
+#OUTDIR="outputs/${FMA}_res${RES}"
+OUTDIR="outputs/${FMA}_highDispersal_logROS"
 RUNNAME="${FMA}_highDispersal_logROS_res${RES}_rep${RUN}"
 RCMD="runName <- '${RUNNAME}'; source('newStart.R')"
 
@@ -20,5 +21,6 @@ do
 done
 
 if [ -f "outputs/${RUNNAME}/rstTimeSinceFire_year1000.tif" ]; then
-  mv "outputs/${RUNNAME}" "${OUTDIR}/res${RES}/rep${RUN}"
+  #mv "outputs/${RUNNAME}" "${OUTDIR}/res${RES}/rep${RUN}"
+  mv "outputs/${RUNNAME}" "${OUTDIR}/rep${RUN}"
 fi
