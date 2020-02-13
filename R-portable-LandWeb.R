de7975af65880ea8f4ee23e647c5f547159f6d31
Alex Chubaty <alex.chubaty@gmail.com>
2020-02-13 08:54:37 -0700

    minor formatting

--- R-portable-LandWeb.R
+++ R-portable-LandWeb.R
@@ -6,13 +6,13 @@ message("library paths:\n", paste("... ", .libPaths(), sep = " ", collapse = "\n
 # chrome.portable = file.path(parentDir,
 #                             "GoogleChromePortable/App/Chrome-bin/chrome.exe")
 # print(chrome.portable)
-# 
+#
 # launch.browser = function(appUrl, browser.path=chrome.portable) {
 #   message("Browser path: ", browser.path)
 #   shell(sprintf("'%s' --app=%s", browser.path, appUrl))
 # }
 
 .libPaths(.libPaths()[2])
-message("library paths:\n", paste("... ", .libPaths(), sep=" ", collapse="\n"))#, getwd()))
+message("library paths:\n", paste("... ", .libPaths(), sep = " ", collapse = "\n"))#, getwd()))
 shiny::runApp(launch.browser = TRUE)
 
