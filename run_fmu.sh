8d1cd3af5fd3538b59f3cc8321c6d329f3adfde1
Alex Chubaty <alex.chubaty@gmail.com>
2020-02-27 10:23:34 -0700

    adjust mv cmd for correct dir structure in fmu script

--- run_fmu.sh
+++ run_fmu.sh
@@ -8,7 +8,8 @@ RES=$3
 printf -v RUN "%02g" $4 ## assign to RUN, padding with extra zeros as needed
 
 OUTDIR1="outputs/FMU_${FMU}_fri${FRI}_res${RES}"
-OUTDIR2="outputs/FMU_${FMU}/fri${FRI}/res${RES}/rep${RUN}"
+OUTDIR2="outputs/FMU_${FMU}/fri${FRI}/res${RES}"
+OUTDIR3="${OUTDIR2}/rep${RUN}"
 RUNNAME="FMU_${FMU}_fri${FRI}_res${RES}_rep${RUN}"
 RCMD="runName <- '${RUNNAME}'; source('newStart.R')"
 
@@ -19,5 +20,5 @@ fi
 echo ${RCMD} | r
 
 if [ -f "outputs/${RUNNAME}/rstTimeSinceFire_year1000.tif" ]; then
-  mv "outputs/${RUNNAME}" ${OUTDIR2}
+  mv "outputs/${RUNNAME}" "${OUTDIR3}"
 fi
