c5009406ae9f86ac9d542696360999f2723bdd1c
Alex Chubaty <achubaty@for-cast.ca>
2020-10-06 14:51:12 -0600

    add v3 run script

---
+++ run_landweb3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## USAGE: ./run_landweb.sh <rep>
+## provide a numeric <rep> as the first and only argument to this script
+
+printf -v RUN "%02g" $1 ## assign to RUN, padding with extra zeros as needed
+
+OUTDIR="outputs/LandWeb_v3"
+RUNNAME="LandWeb_v3_rep${RUN}"
+RCMD="runName <- '${RUNNAME}'; source('newStart.R')"
+
+if [ ! -d ${OUTDIR} ]; then
+  mkdir -p ${OUTDIR}
+fi
+
+echo ${RCMD} | r
+
+if [ -f "outputs/${RUNNAME}/rstTimeSinceFire_year1000.tif" ]; then
+  mv "outputs/${RUNNAME}" "${OUTDIR}/rep${RUN}"
+fi
