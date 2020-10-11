befe3054c46b033382fea11148961beac41c85ef
Alex Chubaty <achubaty@for-cast.ca>
2020-10-11 13:55:06 -0600

    rename newStart.R ==> 00-global.R

--- run_fma_win.sh
+++ run_fma_win.sh
@@ -9,7 +9,7 @@ printf -v RUN "%02g" $2 ## assign to RUN, padding with extra zeros as needed
 OUTDIR="outputs/${FMA}_highDispersal_logROS"
 RUNNAME="${FMA}_highDispersal_logROS_res${RES}_rep${RUN}"
 #RCMD="runName <- '${RUNNAME}'; .libPaths(''); source('newStart.R')"
-RCMD="runName <- '${RUNNAME}'; source('newStart.R')"
+RCMD="runName <- '${RUNNAME}'; source('00-global.R')"
 
 if [ ! -d ${OUTDIR}/res${RES} ]; then
   mkdir -p ${OUTDIR}/res${RES}
