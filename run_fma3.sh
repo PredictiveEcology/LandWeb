f737fbd6adfd5b344c850418e4bb64a576979256
Alex Chubaty <achubaty@for-cast.ca>
2020-10-14 12:56:41 -0600

    add v3 of FMA scripts

---
+++ run_fma3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## USAGE: ./run_fma.sh <fma> <rep>
+
+FMA=$1
+RES=250
+printf -v RUN "%02g" $2 ## assign to RUN, padding with extra zeros as needed
+
+OUTDIR="outputs/${FMA}_v3"
+RUNNAME="${FMA}_v3_res${RES}_rep${RUN}"
+RCMD="runName <- '${RUNNAME}'; source('00-global.R')"
+
+if [ ! -d ${OUTDIR}/res${RES} ]; then
+  mkdir -p ${OUTDIR}/res${RES}
+fi
+
+echo ${RCMD} | r
+
+if [ -f "outputs/${RUNNAME}/rstTimeSinceFire_year1000.tif" ]; then
+  #mv "outputs/${RUNNAME}" "${OUTDIR}/res${RES}/rep${RUN}"
+  mv "outputs/${RUNNAME}" "${OUTDIR}/rep${RUN}"
+fi
