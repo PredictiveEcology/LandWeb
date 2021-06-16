e0459eca31cd0719b4477cdb730bfad8973357cf
Alex Chubaty <achubaty@for-cast.ca>
2021-06-16 11:56:40 -0600

    explicitly use lcc2005

--- 00-global.R
+++ 00-global.R
@@ -14,6 +14,7 @@ if (!require(Require, quietly = TRUE)) {
   install.packages("Require")
   library("Require")
 }
+Require(c("config", "crayon"), require = FALSE)
 
 switch(Sys.info()[["user"]],
        "achubaty" = Sys.setenv(R_CONFIG_ACTIVE = "alex"),
