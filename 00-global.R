57dedc0152b7253a1f10ef00f796592a08517639
Alex Chubaty <achubaty@for-cast.ca>
2022-10-13 15:18:34 -0600

    fix config reference

--- 00-global.R
+++ 00-global.R
@@ -368,7 +368,7 @@ if (config$context[["mode"]] != "postprocess") {
   if (requireNamespace("slackr") & file.exists("~/.slackr")) {
     slackr::slackr_setup()
     slackr::slackr_msg(
-      paste0("Post-processing for `", context$runName, "` completed on host `", Sys.info()[["nodename"]], "`."),
+      paste0("Post-processing for `", config$context[["runName"]], "` completed on host `", Sys.info()[["nodename"]], "`."),
       channel = config$args[["notifications"]][["slackChannel"]], preformatted = FALSE
     )
   }
