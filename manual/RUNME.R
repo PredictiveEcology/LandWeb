644628eea369988d440eb22d399f5a4178282c5b
Alex Chubaty <achubaty@for-cast.ca>
2023-08-18 16:12:37 -0600

    update + rebuild manual

--- manual/RUNME.R
+++ manual/RUNME.R
@@ -1,7 +1,12 @@
 ## this manual must be knitted by running this script
 
 prjDir <- "~/GitHub/LandWeb"
-manDir <- file.path(prjDir, "manual") ## raw files; rendered manual in docs/ (see _bookdown.yml)
+manDir <- file.path(prjDir, "manual") ## raw files; edit these, not the ones in `docsDir`!
+
+docsDir <- file.path(manDir, "_bookdown.yml") |>
+  yaml::read_yaml() |>
+  purrr::pluck("output_dir") |>
+  fs::path_abs()
 
 bibDir <- Require::checkPath(file.path(manDir, "citations"), create = TRUE)
 figDir <- Require::checkPath(file.path(prjDir, "figures"), create = TRUE)
@@ -22,12 +27,12 @@ library("SpaDES.docs")
 ## references ---------------------------------------
 
 ## automatically create a bib database for R packages
-allPkgs <- .packages(all.available = TRUE, lib.loc = .libPaths()[1]) ## TODO: some pkg desc files broken
+allPkgs <- .packages(all.available = TRUE, lib.loc = .libPaths()[1])
 keyPkgs <- c(
   "bookdown", "knitr", "LandR", "LandWebUtils",
   "reproducible", "rmarkdown", "shiny", "SpaDES.core", "SpaDES.tools"
 )
-write_bib(keyPkgs, file.path(bibDir, "packages.bib")) ## TODO: use allPkgs
+write_bib(allPkgs, file.path(bibDir, "packages.bib")) ## TODO: using allPkgs, not all pkgs have dates/years
 
 ## collapse all chapter .bib files into one ------
 bibFiles <- c(
@@ -42,9 +47,9 @@ bibdata <- Reduce(merge, bibdata)
 
 WriteBib(bibdata, file = file.path(bibDir, "references.bib"))
 
-if (!file.exists(file.path(bibDir, "ecology-letters.csl"))) {
-  download.file("https://www.zotero.org/styles/ecology-letters?source=1",
-                destfile = "citations/ecology-letters.csl")
+csl <- file.path(bibDir, "ecology-letters.csl")
+if (!file.exists(csl)) {
+  download.file("https://www.zotero.org/styles/ecology-letters?source=1", destfile = csl)
 }
 
 ## RENDER BOOK ------------------------------------------
@@ -52,29 +57,33 @@ if (!file.exists(file.path(bibDir, "ecology-letters.csl"))) {
 setwd(normalizePath(manDir))
 
 ## prevents GitHub from rendering book using Jekyll
-if (!file.exists(file.path(manDir, ".nojekyll"))) {
-  file.create(file.path(manDir, ".nojekyll"))
+if (!file.exists(file.path(docsDir, ".nojekyll"))) {
+  file.create(file.path(docsDir, ".nojekyll"))
 }
 
 ## set manual version
-Sys.setenv(LANDWEB_MAN_VERSION = read.dcf("../DESCRIPTION")[3]) ## version
+Sys.setenv(LANDWEB_VERSION = read.dcf("../DESCRIPTION")[3]) ## version
+Sys.getenv("LANDWEB_VERSION")
 
 ## don't use Require for package installation etc.
 Sys.setenv(R_USE_REQUIRE = "false")
-
-## render the book using new env -- see <https://stackoverflow.com/a/46083308>
+Sys.getenv("R_USE_REQUIRE")
 
 ## NOTE: need dot because knitting is doing `rm(list = ls())`
 .copyModuleRmds <- prepManualRmds("../m", rebuildCache = FALSE) ## use rel path!
 
+## render the book using new env -- see <https://stackoverflow.com/a/46083308>
 bookdown::render_book(output_format = "all", envir = new.env())
 # bookdown::render_book(output_format = "bookdown::pdf_book", envir = new.env())
 # bookdown::render_book(output_format = "bookdown::bs4_book", envir = new.env())
 
-pdfArchiveDir <- checkPath(file.path("archive", "pdf"), create = TRUE)
-file.copy(from = file.path(manDir, "LandRManual.pdf"),
-          to = file.path(pdfArchiveDir, paste0("LandR-manual-v", Sys.getenv("LANDR_MAN_VERSION"), ".pdf")),
-          overwrite = TRUE)
+pdfArchiveDir <- Require::checkPath(file.path(manDir, "archive", "pdf"), create = TRUE)
+file.copy(
+  from = file.path(docsDir, "LandWeb_manual.pdf"),
+  to = file.path(pdfArchiveDir, paste0("LandWeb-manual-v", Sys.getenv("LANDWEB_VERSION"), ".pdf")),
+  overwrite = TRUE
+)
+file.copy(from = dirname(pdfArchiveDir), to = docsDir, recursive = TRUE)
 
 ## remove temporary .Rmds
 file.remove(.copyModuleRmds)
