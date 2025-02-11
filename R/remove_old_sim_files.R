06dbf5c2e8a2241424b29124647fc1c4189c33af
Alex Chubaty <achubaty@for-cast.ca>
2025-02-10 23:47:54 -0700

    fix/tweak script to rm old sim files

--- R/remove_old_sim_files.R
+++ R/remove_old_sim_files.R
@@ -4,21 +4,17 @@ outputDirs <- c(
   "outputs/Tolko_AB_N_aspenDispersal_logROS"
 )
 
-find_old_files <- function(path, regexp, before = "2024-01-01") {
-  fs::dir_ls(path, regexp = regexp, recurse = 1, type = "file") |>
+lapply(outputDirs, function(d) {
+  fs::dir_ls(d, regexp = paste0("vegTypeMap|standAgeMap|rstTimeSinceFire"), recurse = 1, type = "file") |>
     grep("year([0-9]){3}[.](grd|gri|tif)", x = _, value = TRUE) |>
     fs::file_info() |>
-    dplyr::filter(as.Date(modification_time) < as.Date(!!before)) |>
-    dplyr::pull(path)
-}
-
-lapply(outputDirs, function(d) {
-  find_old_files(d, paste0("vegTypeMap|standAgeMap|rstTimeSinceFire"), "2024-01-01") |>
+    dplyr::filter(as.Date(modification_time) < as.Date("2025-01-15")) |>
+    dplyr::pull(path) |>
     fs::file_delete()
 })
 
 lapply(outputDirs, function(d) {
-  find_old_files(d, paste0(c(
+  fs::dir_ls(d, regexp = paste0(c(
     "Abie_sp[.]tif",
     "Pice_gla[.]tif",
     "Pice_mar[.]tif",
@@ -29,16 +25,23 @@ lapply(outputDirs, function(d) {
     "CASFRIPice_mar[.]tif",
     "CASFRIPinu_sp[.]tif",
     "CASFRIPopu_sp[.]tif"
-  ) , collapse = "|"), "2024-01-01") |>
+  ) , collapse = "|"), recurse = 1, type = "file") |>
+    fs::file_info() |>
+    dplyr::filter(as.Date(modification_time) < as.Date("2024-01-01")) |>
+    dplyr::pull(path) |>
     fs::file_delete()
 })
 
 lapply(outputDirs, function(d) {
-  find_old_files(d, paste0(c(
+  fs::dir_ls(d, regexp = paste0(c(
     "CHECKSUMS[.]txt",
     "mySimOut[.]rds",
+    "mySimOut_0[0-9]00[.]rds",
     "rstFlammable_year1000[.]grd",
     "rstFlammable_year1000[.]gri"
-  ), collapse = "|"), "2024-01-01") |>
+  ) , collapse = "|"), recurse = 1, type = "file") |>
+    fs::file_info() |>
+    dplyr::filter(as.Date(modification_time) < as.Date("2024-01-01")) |>
+    dplyr::pull(path) |>
     fs::file_delete()
 })
