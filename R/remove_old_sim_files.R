bc84b391520e3a8b67da59152163a5702fa2973d
Alex Chubaty <achubaty@for-cast.ca>
2025-03-13 21:32:07 -0600

    update script to find old files
    use `LandWebUtils::findOldSimFiles()`

--- R/remove_old_sim_files.R
+++ R/remove_old_sim_files.R
@@ -1,47 +1,10 @@
-outputDirs <- c(
-  "outputs/BlueRidge_highDispersal_logROS",
-  "outputs/Sundre_highDispersal_logROS",
-  "outputs/Tolko_AB_N_aspenDispersal_logROS"
-)
+outputDirs <- file.path("outputs", c(
+  "BlueRidge_highDispersal_logROS",
+  "Sundre_highDispersal_logROS",
+  "Tolko_AB_N_aspenDispersal_logROS"
+))
 
 lapply(outputDirs, function(d) {
-  fs::dir_ls(d, regexp = paste0("vegTypeMap|standAgeMap|rstTimeSinceFire"), recurse = 1, type = "file") |>
-    grep("year([0-9]){3}[.](grd|gri|tif)", x = _, value = TRUE) |>
-    fs::file_info() |>
-    dplyr::filter(as.Date(modification_time) < as.Date("2025-01-15")) |>
-    dplyr::pull(path) |>
-    fs::file_delete()
-})
-
-lapply(outputDirs, function(d) {
-  fs::dir_ls(d, regexp = paste0(c(
-    "Abie_sp[.]tif",
-    "Pice_gla[.]tif",
-    "Pice_mar[.]tif",
-    "Pinu_sp[.]tif",
-    "Popu_sp[.]tif",
-    "CASFRIAbie_sp[.]tif",
-    "CASFRIPice_gla[.]tif",
-    "CASFRIPice_mar[.]tif",
-    "CASFRIPinu_sp[.]tif",
-    "CASFRIPopu_sp[.]tif"
-  ) , collapse = "|"), recurse = 1, type = "file") |>
-    fs::file_info() |>
-    dplyr::filter(as.Date(modification_time) < as.Date("2024-01-01")) |>
-    dplyr::pull(path) |>
-    fs::file_delete()
-})
-
-lapply(outputDirs, function(d) {
-  fs::dir_ls(d, regexp = paste0(c(
-    "CHECKSUMS[.]txt",
-    "mySimOut[.]rds",
-    "mySimOut_0[0-9]00[.]rds",
-    "rstFlammable_year1000[.]grd",
-    "rstFlammable_year1000[.]gri"
-  ) , collapse = "|"), recurse = 1, type = "file") |>
-    fs::file_info() |>
-    dplyr::filter(as.Date(modification_time) < as.Date("2024-01-01")) |>
-    dplyr::pull(path) |>
+  LandWebUtils::findOldSimFiles(d, before = "2025-01-15")) |>
     fs::file_delete()
 })
