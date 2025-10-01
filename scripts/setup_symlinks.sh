944e58d70835193434b24f3eb7288d6a2bb20e5c
Alex Chubaty <achubaty@for-cast.ca>
2025-10-01 16:16:40 -0600

    add symlink script

---
+++ scripts/setup_symlinks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd ~/GitHub/LandWeb
+
+mkdir -p /mnt/scratch/achubaty/LandWeb/cache
+ln -s /mnt/scratch/achubaty/LandWeb/cache ./
+
+ln -s /mnt/projects/HRV/LandWeb/inputs ./
+ln -s /mnt/projects/HRV/LandWeb/outputs ./
+
